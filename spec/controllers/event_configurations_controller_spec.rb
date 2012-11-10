0825f224c252d9e8c914d75cc24196a36e22d459
Robin Dunlop <rdunlop@thoughtworks.com>
2012-11-10 23:28:56 +0000

    fix broken tests from previous added validations

--- spec/controllers/event_configurations_controller_spec.rb
+++ spec/controllers/event_configurations_controller_spec.rb
@@ -24,7 +24,10 @@ describe EventConfigurationsController do
   # EventConfiguration. As you add validations to EventConfiguration, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      short_name: "something short",
+      long_name: "Something Long"
+    }
   end
 
   # This should return the minimal set of values that should be in the session
