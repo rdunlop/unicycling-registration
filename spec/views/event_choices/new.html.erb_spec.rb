e41e774396ff318212ff8ec0dcdc47d3fccfedcc
Robin Dunlop <rdunlop@thoughtworks.com>
2012-11-25 18:44:40 +0000

    make cell_type into a select dropdown, instead of freeform text

--- spec/views/event_choices/new.html.erb_spec.rb
+++ spec/views/event_choices/new.html.erb_spec.rb
@@ -12,7 +12,7 @@ describe "event_choices/new" do
     assert_select "form", :action => event_choices_path, :method => "post" do
       assert_select "select#event_choice_event_id", :name => "event_choice[event_id]"
       assert_select "input#event_choice_export_name", :name => "event_choice[export_name]"
-      assert_select "input#event_choice_cell_type", :name => "event_choice[cell_type]"
+      assert_select "select#event_choice_cell_type", :name => "event_choice[cell_type]"
       assert_select "input#event_choice_multiple_values", :name => "event_choice[multiple_values]"
       assert_select "input#event_choice_label", :name => "event_choice[label]"
       assert_select "input#event_choice_position", :name => "event_choice[position]"
