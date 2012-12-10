21a05ee570b17bd9c961d6754e63a0dad82fc44a
Robin Dunlop <rdunlop@thoughtworks.com>
2012-12-10 01:49:27 +0000

    remove debug statement

--- spec/views/registration_periods/index.html.erb_spec.rb
+++ spec/views/registration_periods/index.html.erb_spec.rb
@@ -10,7 +10,6 @@ describe "registration_periods/index" do
 
   it "renders a list of registration_periods" do
     render
-    puts rendered
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "100.0", :count => 2
     assert_select "tr>td", :text => "50.0", :count => 2
