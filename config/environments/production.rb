4f53f52849291c45a3db86e35b46af3e2afc7cec
Robin Dunlop <rdunlop@thoughtworks.com>
2013-01-26 19:02:57 +0000

    configure to use the test paypal account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,6 @@ Workspace::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  PAYPAL_BASE_URL = "https://www.paypal.com"
+  #PAYPAL_BASE_URL = "https://www.paypal.com"
+  PAYPAL_BASE_URL = "https://www.sandbox.paypal.com"
 end
