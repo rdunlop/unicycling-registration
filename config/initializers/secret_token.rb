61c514edf41482361236666cbe4718f30ef3350b
Robin Dunlop <rdunlop@thoughtworks.com>
2013-02-04 01:32:15 +0000

    Remove the secret key from the secret.rb file, add instructions for its use

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Workspace::Application.config.secret_token = '9d4e1c3dc4295a00e9a09105d6afeaf2210270f683510a0b5fd3df68e3f29ff4f405588d715d5ce183bcc6094e2138ba4feb5865839068dba26c75b1799c5a7e'
+Workspace::Application.config.secret_token = ENV['SECRET']
