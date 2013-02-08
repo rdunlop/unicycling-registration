a16d1759e0279d6115e9614e5ced8013d5bc2436
Robin Dunlop <rdunlop@thoughtworks.com>
2013-02-08 15:46:59 +0000

    Add the details field to the payments#new, payments#summary pages, which should allow this data to flow through completely now

--- spec/views/payments/new.html.erb_spec.rb
+++ spec/views/payments/new.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "payments/new" do
   before(:each) do
     @payment = FactoryGirl.create(:payment)
+    @pd = FactoryGirl.create(:payment_detail, :payment => @payment, :amount => 10.11, :details => "Hello Werld")
   end
 
   it "renders new payment form" do
@@ -10,6 +11,8 @@ describe "payments/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => payments_path, :method => "post" do
+      assert_select "input#payment_payment_details_attributes_0_amount", :value => @pd.amount
+      assert_select "input#payment_payment_details_attributes_0_details", :value => "DHelo"
     end
   end
 end
