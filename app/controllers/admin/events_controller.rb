de3aa8fc5e6b525f06794086900331ece437b08a
Robin Dunlop <rdunlop@thoughtworks.com>
2013-03-05 03:11:53 +0000

    Fix authorization check on the /admin/events page

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,15 +1,13 @@
 class Admin::EventsController < Admin::BaseController
   before_filter :authenticate_user!
-  skip_authorization_check # XXX because I'm using the BaseController for auth
+  load_and_authorize_resource
 
   def index
-    @events = Event.all
     @num_competitors = Registrant.where({:competitor => true}).count
     @num_non_competitors = Registrant.where({:competitor => false}).count
     @num_registrants = @num_competitors + @num_non_competitors
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 end
