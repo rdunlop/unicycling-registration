900edb777996158d93fc7419afffc534e28da206
Robin Dunlop <rdunlop@thoughtworks.com>
2013-03-15 02:35:56 +0000

    Closes #4. Update the feedback page to include the user's e-mail and registration name, if available

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,16 @@ class WelcomeController < ApplicationController
   end
 
   def feedback
-    Notifications.send_feedback(params[:feedback]).deliver
+    @feedback = params[:feedback]
+    @username = "not-signed-in"
+    @registrants = "unknown"
+    if signed_in?
+      @username = current_user.email
+      if current_user.registrants.count > 0
+        @registrants = current_user.registrants.first.name
+      end
+    end
+    Notifications.send_feedback(@feedback, @username, @registrants).deliver
     respond_to do |format|
       format.html { redirect_to welcome_help_path, notice: 'Feedback sent successfully.' }
     end
