3f6bce080336100915ad12694cfbc592ba990d0a
Robin Dunlop <rdunlop@thoughtworks.com>
2013-03-17 01:18:35 +0000

    add ability to retrieve the correct age_group_entry given an age/gender

--- app/models/age_group_type.rb
+++ app/models/age_group_type.rb
@@ -5,6 +5,12 @@ class AgeGroupType < ActiveRecord::Base
 
   has_many :age_group_entries, :dependent => :destroy
 
+  # Return the age group entry that meets the given age requirements
+  def age_group_entry_for(age, gender)
+    age_group_entries.where("start_age <= :age AND :age <= end_age AND (gender = 'Mixed' OR gender = :gender)", 
+                            :age => age, :gender => gender).first
+  end
+
   def to_s
     name
   end
--- spec/models/age_group_type_spec.rb
+++ spec/models/age_group_type_spec.rb
@@ -7,4 +7,30 @@ describe AgeGroupType do
     agt.name = "Default"
     agt.valid?.should == true
   end
+
+  describe "with a set of age group entries" do
+    before(:each) do
+      @agt = FactoryGirl.create(:age_group_type)
+      @age1 = FactoryGirl.create(:age_group_entry, :age_group_type => @agt, :start_age => 0, :end_age => 10, :gender => "Male")
+      @age2 = FactoryGirl.create(:age_group_entry, :age_group_type => @agt, :start_age => 11, :end_age => 100, :gender => "Male")
+    end
+
+    it "returns nil if no applicable age group entry is found" do
+      @agt.age_group_entry_for(-1, "Male").should == nil
+    end
+
+    it "can return the correct age_group_entry for a given age" do
+      @agt.age_group_entry_for(10, "Male").should == @age1
+    end
+
+    it "returns nil when given a female" do
+      @agt.age_group_entry_for(10, "Female").should == nil
+    end
+
+    it "returns the age group entry if it is configured with 'mixed'" do
+      @age1.gender = "Mixed"
+      @age1.save!
+      @agt.age_group_entry_for(10, "Female").should == @age1
+    end
+  end
 end
