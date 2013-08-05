92f6bc2ad1f42dddfc56a84b7c7e86ec2638aa43
Robin Dunlop <rdunlop@thoughtworks.com>
2013-08-04 22:27:38 -0400

    replace external_ranked_calculator with always-updating version

--- lib/externally_ranked_calculator.rb
+++ lib/externally_ranked_calculator.rb
@@ -4,44 +4,19 @@ class ExternallyRankedCalculator
     @competition = competition
   end
 
+  def get_place_calculator(age_group_desc)
+    @place_calculators ||= {}
+    @place_calculators[age_group_desc] ||= PlaceCalculator.new
+  end
+
   # update the places for all age groups
   def update_all_places
-    age_group_type = @competition.determine_age_group_type
-    unless age_group_type.nil?
-      age_group_type.age_group_entries.each do |age|
-        update_places(age.to_s)
-      end
-    end
-    update_overall_places("Male")
-    update_overall_places("Female")
-  end
+    @competition.external_results.includes(:competitor).order("rank").each do |er|
+      age_place_calc = get_place_calculator(er.competitor.age_group_entry_description)
+      gender_place_calc = get_place_calculator(er.competitor.gender)
 
-
-  private
-  # update the places for an event, fastest first.
-  # DQ are marked as place = 0
-  # results with time==0 are marked as place = 0
-  def update_places(age_group_entry_description)
-    return 0 if age_group_entry_description.nil?
-
-    place_calc = PlaceCalculator.new
-
-    @competition.external_results.order("rank").each do |er|
-
-      # only perform updates on the specified age group entry set
-      next if er.competitor.age_group_entry_description != age_group_entry_description
-
-      er.competitor.place = place_calc.place_next(er.rank, false, er.competitor.ineligible)
-    end
-  end
-
-  def update_overall_places(gender)
-
-    place_calc = PlaceCalculator.new
-    @competition.external_results.order("rank").each do |er|
-      next if er.competitor.gender != gender
-
-      er.competitor.overall_place = place_calc.place_next(er.rank, false, er.competitor.ineligible)
+      er.competitor.place = age_place_calc.place_next(er.rank, false, er.competitor.ineligible)
+      er.competitor.overall_place = gender_place_calc.place_next(er.rank, false, er.competitor.ineligible)
     end
   end
 end
