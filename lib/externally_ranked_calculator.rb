2a006c65e9829007b7b4a9968cebee4fd925e74b
Robin Dunlop <rdunlop@thoughtworks.com>
2013-08-13 11:26:48 -0400

    allow age groups named 'Male' and 'Female' (were affecting the ranking before)

--- lib/externally_ranked_calculator.rb
+++ lib/externally_ranked_calculator.rb
@@ -13,7 +13,7 @@ class ExternallyRankedCalculator
   def update_all_places
     @competition.external_results.includes(:competitor).order("rank").each do |er|
       age_place_calc = get_place_calculator(er.competitor.age_group_entry_description)
-      gender_place_calc = get_place_calculator(er.competitor.gender)
+      gender_place_calc = get_place_calculator("Overall: #{er.competitor.gender}") # differentiate between Overall and an age group named "Male"
 
       er.competitor.place = age_place_calc.place_next(er.rank, false, er.competitor.ineligible)
       er.competitor.overall_place = gender_place_calc.place_next(er.rank, false, er.competitor.ineligible)
