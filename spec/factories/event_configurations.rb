887f822a01d3a2dcbf9623bba6c53dda2be289c0
Robin Dunlop <rdunlop@thoughtworks.com>
2013-08-24 18:52:15 -0400

    remove default value for event_configuration#currency to fix test

--- spec/factories/event_configurations.rb
+++ spec/factories/event_configurations.rb
@@ -9,7 +9,7 @@ FactoryGirl.define do
     event_url "http://www.naucc.com"
     start_date Date.new(2013,1,1)
     #logo ""
-    currency "$"
+    currency nil
     contact_email "robinc@dunlopweb.com"
     artistic_closed_date "2013-1-10"
     standard_skill_closed_date "2013-3-10"
