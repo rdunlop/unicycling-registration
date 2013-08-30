a44f4970a51b6b1619c838d538e0cbc2796d33e6
Robin Dunlop <rdunlop@thoughtworks.com>
2013-08-30 17:40:50 -0400

    start displaying 'free' items correctly

--- app/models/registrant_expense_item.rb
+++ app/models/registrant_expense_item.rb
@@ -1,4 +1,6 @@
 class RegistrantExpenseItem < ActiveRecord::Base
+  include ApplicationHelper # for print_formatted_currency
+
   attr_accessible :expense_item_id, :registrant_id, :details, :free
 
   belongs_to :registrant
@@ -8,5 +10,24 @@ class RegistrantExpenseItem < ActiveRecord::Base
 
   validates :expense_item, :registrant, :presence => true
   validates_associated :registrant
+
+
+  def cost_s
+    return "Free" if free
+
+    print_formatted_currency(expense_item.cost)
+  end
+
+  def cost
+    return 0 if free
+
+    expense_item.cost
+  end
+
+  def tax
+    return 0 if free
+
+    expense_item.tax
+  end
 
 end
