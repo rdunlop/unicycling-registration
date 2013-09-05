c34fa5d892d31fe27ef375c4bf0270176a2feea6
Robin Dunlop <rdunlop@thoughtworks.com>
2013-09-04 22:23:39 -0400

    fix broken unit test of expense_group view

--- spec/views/expense_groups/index.html.erb_spec.rb
+++ spec/views/expense_groups/index.html.erb_spec.rb
@@ -6,13 +6,13 @@ describe "expense_groups/index" do
     assign(:expense_groups, [
       stub_model(ExpenseGroup,
         :group_name => "Group Name",
-        :visible => false,
+        :visible => true,
         :info_url => "hello world",
         :position => 1
       ),
       stub_model(ExpenseGroup,
         :group_name => "Group Name",
-        :visible => false,
+        :visible => true,
         :position => 1
       )
     ])
@@ -23,7 +23,7 @@ describe "expense_groups/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Group Name".to_s, :count => 2
     assert_select "tr>td", :text => "hello world".to_s, :count => 1
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", :text => true.to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
   end
 end
