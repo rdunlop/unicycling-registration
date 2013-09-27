9c8c0953b7e881163d4763ea6bf00470cd51b764
Robin Dunlop <rdunlop@thoughtworks.com>
2013-09-27 18:02:08 -0400

    fix tests

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -20,23 +20,26 @@ describe Notifications do
   end
 
   describe "payment_completed" do
-    let(:payment) { FactoryGirl.create(:payment) }
+    let(:payment) { FactoryGirl.create(:payment, :completed => true) }
     let!(:payment_detail) { FactoryGirl.create(:payment_detail, :amount => 10, :payment => payment) }
-    let(:mail) { Notifications.payment_completed(payment) }
+    before(:each) do
+      payment.reload
+      ENV['PAYMENT_NOTICE_EMAIL'] = "robin+p@dunlopweb.com"
+      @mail = Notifications.payment_completed(payment)
+    end
 
     it "renders the headers" do
-      ENV['PAYMENT_NOTICE_EMAIL'] = "robin+p@dunlopweb.com"
-      mail.subject.should eq("Payment completed")
-      mail.to.should eq([payment.user.email])
-      mail.bcc.should eq(["robin+p@dunlopweb.com"])
-      mail.from.should eq(["from@example.com"])
+      @mail.subject.should eq("Payment completed")
+      @mail.to.should eq([payment.user.email])
+      @mail.bcc.should eq(["robin+p@dunlopweb.com"])
+      @mail.from.should eq(["from@example.com"])
     end
 
     it "assigns the total_amount" do
-      mail.body.should match(/A payment for \$10.00 USD has been received/)
+      @mail.body.should match(/A payment for \$10.00 USD has been received/)
     end
     it "assigns the full-event-name to @event_name" do
-      mail.body.should match(/NAUCC 2140 - Payment Received/)
+      @mail.body.should match(/NAUCC 2140 - Payment Received/)
     end
   end
 
