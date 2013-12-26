654e2b34d5ff314326ed7e758cd81811ebfc5b8b
Robin Dunlop <rdunlop@thoughtworks.com>
2013-12-25 19:57:51 -0500

    fix tests broken by recent emaile changes

--- spec/controllers/admin/registrants_controller_spec.rb
+++ spec/controllers/admin/registrants_controller_spec.rb
@@ -46,8 +46,9 @@ describe Admin::RegistrantsController do
 
   describe "POST send_email" do
     it "can send an e-mail" do
+      FactoryGirl.create(:user)
       ActionMailer::Base.deliveries.clear
-      post :send_email, { :email => {:subject => "Hello werld", :body => "This is the body", :email_addresses => ["robin@dunlopweb.com", "robin+1@dunlopweb.com"] }}
+      post :send_email, { :email => {:subject => "Hello werld", :body => "This is the body", :confirmed_accounts => true }}
       num_deliveries = ActionMailer::Base.deliveries.size
       num_deliveries.should == 1
       message = ActionMailer::Base.deliveries.first
@@ -55,12 +56,11 @@ describe Admin::RegistrantsController do
     end
 
     it "breaks apart large requests into multiple smaller requests" do
-      ActionMailer::Base.deliveries.clear
-      addresses = []
       50.times do |n|
-        addresses << "robin+#{n}@dunlopweb.com"
+        FactoryGirl.create(:user)
       end
-      post :send_email, { :email => {:subject => "Hello werld", :body => "This is the body", :email_addresses => addresses }}
+      ActionMailer::Base.deliveries.clear
+      post :send_email, { :email => {:subject => "Hello werld", :body => "This is the body", :confirmed_accounts => true }}
       num_deliveries = ActionMailer::Base.deliveries.size
       num_deliveries.should == 2
 
@@ -68,7 +68,7 @@ describe Admin::RegistrantsController do
       first_message.bcc.count.should == 30
 
       second_message = ActionMailer::Base.deliveries.second
-      second_message.bcc.count.should == 20
+      second_message.bcc.count.should == 21
     end
   end
 end
