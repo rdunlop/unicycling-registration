6f9af2f8084cea677f9d9b874f80740f835a5f68
Robin Dunlop <rdunlop@thoughtworks.com>
2013-12-27 11:14:07 -0500

    update emailer to allow e-mailing either all, paid, or unpaid groups

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,6 +4,7 @@ class Email
   include ActiveModel::Conversion
 
   attribute :confirmed_accounts, Boolean
+  attribute :paid_reg_accounts, Boolean
   attribute :unpaid_reg_accounts, Boolean
   attr_accessor :subject, :body
   validates_presence_of :subject, :body
@@ -14,12 +15,27 @@ class Email
     end
   end
 
+  def self.addresses_for_confirmed_accounts
+    return User.confirmed.map{|user| user.email }
+  end
+
+  def self.addresses_for_paid_reg_accounts
+    self.addresses_for_confirmed_accounts - self.addresses_for_unpaid_reg_accounts
+  end
+
+  def self.addresses_for_unpaid_reg_accounts
+    return User.unpaid_reg_fees.map{|user| user.email }
+  end
+
   def email_addresses
     if self.confirmed_accounts
-      return User.confirmed.map{|user| user.email }
-    end
-    if self.unpaid_reg_accounts
-      return User.unpaid_reg_fees.map{|user| user.email }
+      Email.addresses_for_confirmed_accounts
+    elsif self.paid_reg_accounts
+      Email.addresses_for_paid_reg_accounts
+    elsif self.unpaid_reg_accounts
+      Email.addresses_for_unpaid_reg_accounts
+    else
+      []
     end
   end
 
