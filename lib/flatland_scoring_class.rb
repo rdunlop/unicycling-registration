1404f2f7cb2b36d14813b031cbbd0f34222260d2
Robin Dunlop <rdunlop@thoughtworks.com>
2014-01-30 21:20:20 -0500

    add scoring helpers

---
+++ lib/flatland_scoring_class.rb
@@ -0,0 +1,58 @@
+class FlatlandScoringClass
+  include Rails.application.routes.url_helpers
+
+  def initialize(competition)
+    @competition = competition
+  end
+
+
+  # This is used temporarily to access the calculator, but will likely be private-ized soon
+  def score_calculator
+    FlatlandScoreCalculator.new(@competition)
+  end
+
+  # describes how to label the results of this competition
+  def result_description
+    nil
+  end
+
+  # describes whether the given competitor has any results associated
+  def competitor_has_result?(competitor)
+    false
+  end
+
+  # returns the result for this competitor
+  def competitor_result(competitor)
+    if self.competitor_has_result?(competitor)
+      nil# not applicable in Freestyle
+    else
+      nil
+    end
+  end
+
+  # Function which places all of the competitors in the competition
+  def place_all
+    nil
+  end
+
+
+  # Used when trying to destroy all results for a competition
+  def all_competitor_results
+    nil
+  end
+
+  # the page where all of the results for this competition are listed
+  def results_path
+    nil
+  end
+
+  ########### Below this line, the entries are not (YET) used
+
+  # for award_label
+  def create_import_result_from_csv(line)
+  end
+
+  # for award_label
+  def create_result_from_import_result(import_result)
+  end
+end
