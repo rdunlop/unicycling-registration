7c46760c41799d22a1f53fdc9db0195feb054706
Robin Dunlop <rdunlop@thoughtworks.com>
2014-02-10 03:39:27 -0500

    fix award_label and events#show tests

--- spec/views/award_labels/edit.html.erb_spec.rb
+++ spec/views/award_labels/edit.html.erb_spec.rb
@@ -3,20 +3,7 @@ require 'spec_helper'
 describe "award_labels/edit" do
   before(:each) do
     @user = FactoryGirl.create(:admin_user)
-    @award_label = FactoryGirl.create(:award_label,
-      :bib_number => 1,
-      :first_name => "MyString",
-      :last_name => "MyString",
-      :partner_first_name => "MyString",
-      :partner_last_name => "MyString",
-      :competition_name => "MyString",
-      :team_name => "MyString",
-      :age_group => "MyString",
-      :gender => "MyString",
-      :details => "MyString",
-      :place => 1,
-      :registrant_id => 1
-    )
+    @award_label = FactoryGirl.create(:award_label)
   end
 
   it "renders the edit award_label form" do
@@ -25,14 +12,10 @@ describe "award_labels/edit" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => award_label_path(@award_label), :method => "put" do
       assert_select "input#award_label_bib_number", :name => "award_label[bib_number]"
-      assert_select "input#award_label_first_name", :name => "award_label[first_name]"
-      assert_select "input#award_label_last_name", :name => "award_label[last_name]"
-      assert_select "input#award_label_partner_first_name", :name => "award_label[partner_first_name]"
-      assert_select "input#award_label_partner_last_name", :name => "award_label[partner_last_name]"
+      assert_select "input#award_label_competitor_name", :name => "award_label[competitor_name]"
       assert_select "input#award_label_competition_name", :name => "award_label[competition_name]"
       assert_select "input#award_label_team_name", :name => "award_label[team_name]"
-      assert_select "input#award_label_age_group", :name => "award_label[age_group]"
-      assert_select "input#award_label_gender", :name => "award_label[gender]"
+      assert_select "input#award_label_category", :name => "award_label[category]"
       assert_select "input#award_label_details", :name => "award_label[details]"
       assert_select "input#award_label_place", :name => "award_label[place]"
       assert_select "select#award_label_registrant_id", :name => "award_label[registrant_id]"
--- spec/views/award_labels/index.html.erb_spec.rb
+++ spec/views/award_labels/index.html.erb_spec.rb
@@ -7,14 +7,10 @@ describe "award_labels/index" do
     assign(:award_labels, [
       FactoryGirl.create(:award_label,
         :bib_number => 123,
-        :first_name => "Robin",
-        :last_name => "Dunlop",
-        :partner_first_name => "Connie",
-        :partner_last_name => "Cotter",
+        :competitor_name => "Robin Dunlop & Connie Cotter",
         :competition_name => "Pairs Freestyle",
         :team_name => "TCUC",
-        :age_group => "All",
-        :gender => "(mixed)",
+        :category => "Adults",
         :details => "Winner",
         :place => 2
       ),
@@ -29,26 +25,14 @@ describe "award_labels/index" do
     assert_select "tr>td", :text => "Robin Dunlop &amp; Connie Cotter".to_s, :count => 1
     assert_select "tr>td", :text => "Pairs Freestyle".to_s, :count => 1
     assert_select "tr>td", :text => "TCUC".to_s, :count => 1
-    assert_select "tr>td", :text => "All".to_s, :count => 1
+    assert_select "tr>td", :text => "Adults".to_s, :count => 1
     assert_select "tr>td", :text => "Winner".to_s, :count => 1
     assert_select "tr>td", :text => "2nd Place".to_s, :count => 1
     assert_select "tr>td", :text => 2.to_s, :count => 1
   end
   describe "award_labels/new" do
     before(:each) do
-      assign(:award_label, FactoryGirl.build(:award_label,
-        :bib_number => 1,
-        :first_name => "MyString",
-        :last_name => "MyString",
-        :partner_first_name => "MyString",
-        :partner_last_name => "MyString",
-        :competition_name => "MyString",
-        :team_name => "MyString",
-        :age_group => "MyString",
-        :gender => "MyString",
-        :details => "MyString",
-        :place => 1,
-      ))
+      assign(:award_label, FactoryGirl.build(:award_label))
     end
   
     it "renders new award_label form" do
@@ -57,14 +41,10 @@ describe "award_labels/index" do
       # Run the generator again with the --webrat flag if you want to use webrat matchers
       assert_select "form", :action => user_award_labels_path(@user), :method => "post" do
         assert_select "input#award_label_bib_number", :name => "award_label[bib_number]"
-        assert_select "input#award_label_first_name", :name => "award_label[first_name]"
-        assert_select "input#award_label_last_name", :name => "award_label[last_name]"
-        assert_select "input#award_label_partner_first_name", :name => "award_label[partner_first_name]"
-        assert_select "input#award_label_partner_last_name", :name => "award_label[partner_last_name]"
+        assert_select "input#award_label_competitor_name", :name => "award_label[competitor_name]"
         assert_select "input#award_label_competition_name", :name => "award_label[competition_name]"
         assert_select "input#award_label_team_name", :name => "award_label[team_name]"
-        assert_select "input#award_label_age_group", :name => "award_label[age_group]"
-        assert_select "input#award_label_gender", :name => "award_label[gender]"
+        assert_select "input#award_label_category", :name => "award_label[category]"
         assert_select "input#award_label_details", :name => "award_label[details]"
         assert_select "input#award_label_place", :name => "award_label[place]"
         assert_select "select#award_label_registrant_id", :name => "award_label[registrant_id]"
