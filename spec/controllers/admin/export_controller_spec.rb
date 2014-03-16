64b24701d9a3317ed0d443b807aa0724ad204fd5
Robin Dunlop <robin@dunlopweb.com>
2014-03-16 11:56:22 -0500

    update admin/exports_controller format to be pdf

--- spec/controllers/admin/export_controller_spec.rb
+++ spec/controllers/admin/export_controller_spec.rb
@@ -268,7 +268,7 @@ describe Admin::ExportController do
 
   describe "GET download_events" do
     it "without any events or registrants, only prints the headers" do
-      get :download_events, {:format => 'pdf' }
+      get :download_events, {:format => 'xls' }
       assigns(:data).should == [["Registrant Name", "Age", "Gender"]]
     end
     describe "with some events defined" do
@@ -276,12 +276,12 @@ describe Admin::ExportController do
         @ev = FactoryGirl.create(:event)
       end
       it "lists the event titles" do
-        get :download_events, {:format => 'pdf' }
+        get :download_events, {:format => 'xls' }
         data = assigns(:data)
         data[0].should == ["Registrant Name", "Age", "Gender", @ev.event_categories.first.to_s]
       end
       it "lists the each event_choice separately, with event-prefixed" do
-        get :download_events, {:format => 'pdf' }
+        get :download_events, {:format => 'xls' }
         ec = EventCategory.all.first
         data = assigns(:data)
         data[0].should == ["Registrant Name", "Age", "Gender", ec.to_s]
@@ -292,7 +292,7 @@ describe Admin::ExportController do
           @reg = FactoryGirl.create(:competitor)
         end
         it "has a row for that competitor" do
-          get :download_events, {:format => 'pdf' }
+          get :download_events, {:format => 'xls' }
           data = assigns(:data)
           data[1].should == [@reg.name, @reg.age, @reg.gender, nil]
         end
@@ -302,7 +302,7 @@ describe Admin::ExportController do
             @rc = FactoryGirl.create(:registrant_event_sign_up, :registrant => @reg, :event_category => @ecat, :event => @ev, :signed_up => true)
           end
           it "has a value in the target column" do
-            get :download_events, {:format => 'pdf' }
+            get :download_events, {:format => 'xls' }
             data = assigns(:data)
             data[1][3].should == true
           end
