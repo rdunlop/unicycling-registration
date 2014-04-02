b1f59d10188308b7fe9e064008786c03d1d030c0
Robin Dunlop <robin@dunlopweb.com>
2014-04-01 19:20:06 -0500

    optimize query fetch of registrants->users

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -29,7 +29,7 @@ class RegistrantsController < ApplicationController
   public
 
   def all_index
-    @registrants = Registrant.unscoped
+    @registrants = Registrant.unscoped.includes(:user)
   end
 
   # GET /registrants
