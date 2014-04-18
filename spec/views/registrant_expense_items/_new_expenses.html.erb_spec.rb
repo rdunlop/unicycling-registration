0fe60fe5d58021df88fff4ea9ef6704cb5a2f16d
Robin Dunlop <robin@dunlopweb.com>
2014-04-18 16:51:03 -0500

    more stubbing

--- spec/views/registrant_expense_items/_new_expenses.html.erb_spec.rb
+++ spec/views/registrant_expense_items/_new_expenses.html.erb_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe "registrant_expense_items/_new_expenses" do
   before(:each) do
-    @group = FactoryGirl.create(:expense_group)
-    @item1 = FactoryGirl.create(:expense_item, :expense_group => @group)
-    @item2 = FactoryGirl.create(:expense_item, :expense_group => @group)
+    @group = FactoryGirl.build_stubbed(:expense_group)
+    @item1 = FactoryGirl.build_stubbed(:expense_item, :expense_group => @group)
+    @item2 = FactoryGirl.build_stubbed(:expense_item, :expense_group => @group)
+    allow(@group).to receive(:expense_items).and_return([@item1, @item2])
 
-    @group2 = FactoryGirl.create(:expense_group, :position => 2, :visible => false)
-    @item3 = FactoryGirl.create(:expense_item, :expense_group => @group2)
-    @item4 = FactoryGirl.create(:expense_item, :expense_group => @group2)
-    @registrant = FactoryGirl.create(:competitor)
+    @group2 = FactoryGirl.build_stubbed(:expense_group, :position => 2, :visible => false)
+    @item3 = FactoryGirl.build_stubbed(:expense_item, :expense_group => @group2)
+    @item4 = FactoryGirl.build_stubbed(:expense_item, :expense_group => @group2)
+    allow(@group2).to receive(:expense_items).and_return([@item3, @item4])
+
+    expect(ExpenseGroup).to receive_message_chain(:visible, :includes).and_return([@group])
+    @registrant = FactoryGirl.build_stubbed(:competitor)
   end
 
   it "renders group header" do
