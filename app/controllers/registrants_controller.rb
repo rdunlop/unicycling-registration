6a93c9113330643e8a10a2bd31b733e43409bfbc
Robin Dunlop <robin@dunlopweb.com>
2014-05-03 18:41:56 -0500

    eager load show_all associations

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -283,7 +283,7 @@ class RegistrantsController < ApplicationController
   end
 
   def show_all
-    @registrants = Registrant.active.order(:last_name, :first_name)
+    @registrants = Registrant.active.order(:last_name, :first_name).includes(:contact_detail, :registrant_expense_items, :registrant_event_sign_ups)
 
     respond_to do |format|
       format.html # show_all.html.erb
