2f997a0c57015145a57246f54095de144d826431
Robin Dunlop <robin@dunlopweb.com>
2014-05-07 23:42:48 -0500

    fix tests

--- spec/models/two_attempt_entry_spec.rb
+++ spec/models/two_attempt_entry_spec.rb
@@ -4,19 +4,20 @@ describe TwoAttemptEntry do
 
   describe "building array from ImportResults" do
     let(:user) { FactoryGirl.create(:user) }
+    let(:is_start_time) { true }
     let(:bib_number1) { 1123 }
     let(:bib_number2) { 111 }
     let(:competition) { FactoryGirl.create(:competition) }
 
     before :each do
-      i1a = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number1)
-      i1b = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number1)
-      i2a = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number2)
-      i2b = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number2)
+      i1a = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number1, is_start_time: is_start_time)
+      i1b = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number1, is_start_time: is_start_time)
+      i2a = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number2, is_start_time: is_start_time)
+      i2b = FactoryGirl.create(:import_result, user: user, competition: competition, bib_number: bib_number2, is_start_time: is_start_time)
     end
 
     it "can create an array" do
-      entries = TwoAttemptEntry.entries_for(user, competition)
+      entries = TwoAttemptEntry.entries_for(user, competition, is_start_time)
       expect(entries.length).to eq(2)
       expect(entries.map(&:bib_number)).to match_array([bib_number1, bib_number2])
     end
