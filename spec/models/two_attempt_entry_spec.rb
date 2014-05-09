9e036b798dd9dfdacfe569b9f186ef09821aa1be
Robin Dunlop <robin@dunlopweb.com>
2014-05-09 18:51:44 -0500

    add more tests for two_attempt_entry

--- spec/models/two_attempt_entry_spec.rb
+++ spec/models/two_attempt_entry_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe TwoAttemptEntry do
+  let(:competition) { FactoryGirl.create(:competition) }
+  let(:competitor1) { FactoryGirl.create(:event_competitor, competition: competition) }
+  let(:competitor2) { FactoryGirl.create(:event_competitor, competition: competition) }
+  let(:user) { FactoryGirl.create(:user) }
 
   describe "building array from ImportResults" do
-    let(:user) { FactoryGirl.create(:user) }
     let(:is_start_time) { true }
     let(:bib_number1) { 1123 }
     let(:bib_number2) { 111 }
@@ -20,6 +23,28 @@ describe TwoAttemptEntry do
       entries = TwoAttemptEntry.entries_for(user, competition, is_start_time)
       expect(entries.length).to eq(2)
       expect(entries.map(&:bib_number)).to match_array([bib_number1, bib_number2])
+    end
+  end
+
+  describe "creating time_results from TwoAttemptEntry" do
+    let(:two_attempt_entry) { TwoAttemptEntry.new(
+      user: user,
+      bib_number: competitor1.bib_number,
+      competition: competition,
+
+      minutes_1: 1,
+      seconds_1: 2,
+      thousands_1: 3,
+      dq_1: false,
+
+      minutes_2: 4,
+      seconds_2: 5,
+      thousands_2: 6,
+      dq_2: true
+      ) }
+    it "can save to the database" do
+      expect(two_attempt_entry.save).to eq(true)
+      expect(ImportResult.count).to eq(2)
     end
   end
 end
