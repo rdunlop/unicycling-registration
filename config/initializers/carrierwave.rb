cf635d82ec05e9de2eaad04cb30dd115c82eb29f
Robin Dunlop <robin@dunlopweb.com>
2014-06-03 22:30:38 -0500

    upgrade carrierwave_direct to fix key-match errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,6 @@ CarrierWave.configure do |config|
         provider: "AWS",
         aws_access_key_id: ENV['AWS_ACCESS_KEY'],
         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-        #region: "us-west-1",
-        #endpoint: "http://robind.s3-us-west-2.amazonaws.com"
     }
     config.fog_directory = ENV['AWS_BUCKET']
     config.fog_attributes = {
