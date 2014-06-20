7e63e1c13988cffc8023b7ea8d3657cb9c7aad0a
Robin Dunlop <robin@dunlopweb.com>
2014-06-19 23:20:52 -0500

    style the data_entry_menu to reduce distractions

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,18 +1,18 @@
 module EventsHelper
-    def scoring_link(judge)
+    def scoring_link(judge, options = {})
         class_name = judge.competition.event_class
 
         case class_name
         when 'Two Attempt Distance'
-          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_distance_attempts_path(judge) 
+          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_distance_attempts_path(judge), options
         when 'Freestyle'
-          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_scores_path(judge) 
+          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_scores_path(judge), options
         when 'Flatland'
-            link_to judge.judge_type.name, competition_competitors_path(judge.competition)
+            link_to judge.judge_type.name, competition_competitors_path(judge.competition), options
         when 'Street'
-            link_to judge.judge_type.name, judge_street_scores_path(judge)
+            link_to judge.judge_type.name, judge_street_scores_path(judge), options
         when 'Standard'
-            link_to judge.judge_type.name, competition_standard_scores_path(judge.competition)
+            link_to judge.judge_type.name, competition_standard_scores_path(judge.competition), options
         else
             "please update the scoring_link function (#{judge.competition.event_class})"
         end
