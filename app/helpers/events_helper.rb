313734ab02d40b3cd2ffb51a0ebca0b4b36aba83
Robin Dunlop <robin@dunlopweb.com>
2014-06-25 22:03:22 -0500

    change Is Start Time to 'Start Line Data'/'Finish Line Data'

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,20 +1,25 @@
 module EventsHelper
-    def scoring_link(judge, options = {})
-        class_name = judge.competition.event_class
+  def scoring_link(judge, options = {})
+    class_name = judge.competition.event_class
 
-        case class_name
-        when 'Two Attempt Distance'
-          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_distance_attempts_path(judge), options
-        when 'Freestyle'
-          link_to "#{judge.competition} - #{judge.judge_type.name}", judge_scores_path(judge), options
-        when 'Flatland'
-            link_to judge.judge_type.name, competition_competitors_path(judge.competition), options
-        when 'Street'
-            link_to judge.judge_type.name, judge_street_scores_path(judge), options
-        when 'Standard'
-            link_to judge.judge_type.name, competition_standard_scores_path(judge.competition), options
-        else
-            "please update the scoring_link function (#{judge.competition.event_class})"
-        end
+    case class_name
+    when 'Two Attempt Distance'
+      link_to "#{judge.competition} - #{judge.judge_type.name}", judge_distance_attempts_path(judge), options
+    when 'Freestyle'
+      link_to "#{judge.competition} - #{judge.judge_type.name}", judge_scores_path(judge), options
+    when 'Flatland'
+      link_to judge.judge_type.name, competition_competitors_path(judge.competition), options
+    when 'Street'
+      link_to judge.judge_type.name, judge_street_scores_path(judge), options
+    when 'Standard'
+      link_to judge.judge_type.name, competition_standard_scores_path(judge.competition), options
+    else
+      "please update the scoring_link function (#{judge.competition.event_class})"
     end
+  end
+
+  def start_finish_description(is_start_time)
+    is_start_time = false if is_start_time == "false"
+    is_start_time ? "Start Line Data" : "Finish Line Data"
+  end
 end
