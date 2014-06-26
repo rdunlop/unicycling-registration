2c88dc4696a1dfeb85f3f609f27f69461ddf268c
Robin Dunlop <robin@dunlopweb.com>
2014-06-26 17:31:39 -0500

    update the judge type

--- db/migrate/20140626222054_change_name_of_distance_to_high_long.rb
+++ db/migrate/20140626222054_change_name_of_distance_to_high_long.rb
@@ -2,21 +2,29 @@ class ChangeNameOfDistanceToHighLong < ActiveRecord::Migration
   class Competition < ActiveRecord::Base
   end
 
+  class JudgeType < ActiveRecord::Base
+  end
+
   def up
     Competition.reset_column_information
-    Competition.all.each do |c|
-      if c.scoring_class == "Two Attempt Distance"
-        c.update_attribute(:scoring_class,  "High/Long")
-      end
+    Competition.where(scoring_class: "Two Attempt Distance").each do |c|
+      c.update_attribute(:scoring_class,  "High/Long")
+    end
+    JudgeType.reset_column_information
+    JudgeType.where(event_class: "Two Attempt Distance").each do |j|
+      j.update_attribute(:event_class, "High/Long")
+      j.update_attribute(:name, "High/Long Judge Type")
     end
   end
 
   def down
     Competition.reset_column_information
-    Competition.all.each do |c|
-      if c.scoring_class == "High/Long"
-        c.update_attribute(:scoring_class,  "Two Attempt Distance")
-      end
+    Competition.where(scoring_class: "High/Long").each do |c|
+      c.update_attribute(:scoring_class,  "Two Attempt Distance")
+    end
+    JudgeType.where(event_class: "High/Long").each do |j|
+      j.update_attribute(:event_class, "Two Attempt Distance")
+      j.update_attribute(:name, "Distance Judge Type")
     end
   end
 end
