bb388eb27e7aa9d6cc74ee07d9e868a7b712f3f3
Robin Dunlop <robin@dunlopweb.com>
2014-06-26 17:25:29 -0500

    run migration to change competition data

--- db/migrate/20140626222054_change_name_of_distance_to_high_long.rb
+++ db/migrate/20140626222054_change_name_of_distance_to_high_long.rb
@@ -1,11 +1,21 @@
 class ChangeNameOfDistanceToHighLong < ActiveRecord::Migration
   class Competition < ActiveRecord::Base
   end
-  def change
+
+  def up
     Competition.reset_column_information
-    Competition.each do |c|
+    Competition.all.each do |c|
       if c.scoring_class == "Two Attempt Distance"
         c.update_attribute(:scoring_class,  "High/Long")
+      end
+    end
+  end
+
+  def down
+    Competition.reset_column_information
+    Competition.all.each do |c|
+      if c.scoring_class == "High/Long"
+        c.update_attribute(:scoring_class,  "Two Attempt Distance")
       end
     end
   end
