f7ac397286f6b9ee45208c68fb6ec80ca094ec6e
Robin Dunlop <robin@dunlopweb.com>
2014-08-04 19:19:57 -0400

    allow choosing multiple competitions for the emailer

--- app/models/email.rb
+++ app/models/email.rb
@@ -8,7 +8,7 @@ class Email
   attribute :paid_reg_accounts, Boolean
   attribute :unpaid_reg_accounts, Boolean
   attribute :no_reg_accounts, Boolean
-  attribute :competition_id, Integer
+  attribute :competition_id, Array
   attribute :category_id, Integer
 
   attr_accessor :subject, :body
@@ -20,8 +20,13 @@ class Email
     end
   end
 
-  def competition
-    Competition.find(competition_id) if competition_id.present?
+  def competitions
+    return @competitions if @competitions
+    @competitions = []
+    competition_id.each do |cid|
+      @competitions << Competition.find(cid) if cid.present?
+    end
+    @competitions
   end
 
   def category
@@ -37,8 +42,8 @@ class Email
       "User Accounts with ANY Registrants who have NOT Paid Reg Fees"
     elsif no_reg_accounts
       "User Accounts with No Registrants"
-    elsif competition_id.present?
-      "Emails of users/registrants associated with #{competition}"
+    elsif competition_id.any?
+      "Emails of users/registrants associated with #{competitions.map(&:to_s).join(' ')}"
     elsif category_id.present?
       "Emails of users/registrants associated with any competition in #{category}"
     else
@@ -55,8 +60,8 @@ class Email
       User.unpaid_reg_fees.map{|user| user.email }
     elsif no_reg_accounts
       (User.confirmed - User.all_with_registrants).map{|user| user.email }
-    elsif competition_id.present?
-      competition.registrants.map(&:user).map(&:email).compact.uniq
+    elsif competition_id.any?
+      competitions.map(&:registrants).flatten.map(&:user).map(&:email).compact.uniq
     elsif category_id.present?
       category.events.map(&:competitor_registrants).flatten.map(&:user).map(&:email).compact.uniq
     else
@@ -65,8 +70,8 @@ class Email
   end
 
   def filtered_registrant_emails
-    if competition_id.present?
-      competition.registrants.map(&:contact_detail).map(&:email).compact.uniq
+    if competition_id.any?
+      competitions.map(&:registrants).flatten.map(&:contact_detail).map(&:email).compact.uniq
     elsif category_id.present?
       category.events.map(&:competitor_registrants).flatten.map(&:contact_detail).map(&:email).compact.uniq
     else
