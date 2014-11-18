1337ae375b2edcc775f94ec2edeae5589dc933da
Robin Dunlop <robin@dunlopweb.com>
2014-11-17 23:03:57 -0600

    fix coupon spec

--- spec/actions/coupon_applier_spec.rb
+++ spec/actions/coupon_applier_spec.rb
@@ -6,7 +6,7 @@ describe CouponApplier do
   let(:coupon_code) { FactoryGirl.create(:coupon_code, max_num_uses: max_coupon_uses) }
   let!(:coupon_code_detail) { FactoryGirl.create(:coupon_code_expense_item, coupon_code: coupon_code, expense_item: expense_item)}
   let(:coupon_code_string) { coupon_code.code }
-  let(:payment) { FactoryGirl.create(:payment) }
+  let(:payment) { FactoryGirl.create(:payment, :completed) }
   let!(:payment_detail) { FactoryGirl.create(:payment_detail, payment: payment, expense_item: expense_item) }
   let(:subject) { described_class.new(payment.reload, coupon_code_string) }
   let(:do_action) { subject.perform }
