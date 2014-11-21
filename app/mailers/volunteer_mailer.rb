309533e31ca70daca60c9adee4c94f480a7b23e5
Robin Dunlop <robin@dunlopweb.com>
2014-11-20 16:23:36 -0800

    remove unused ipn_received from Volunteer Mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,12 +1,6 @@
 class VolunteerMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
 
-  def ipn_received(contents)
-    @contents = contents
-
-    mail to: ENV['ERROR_EMAIL']
-  end
-
   def new_volunteer(volunteer_choice_id)
     volunteer_choice =  VolunteerChoice.find(volunteer_choice_id)
     @registrant = volunteer_choice.registrant
