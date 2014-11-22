9fec7616d0f0bd72c53af9f321ab0c9aa804521b
Robin Dunlop <robin@dunlopweb.com>
2014-11-22 11:43:16 -0600

    change production to use redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,9 @@ Workspace::Application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+  if Rails.application.secrets.redis
+    config.cache_store = :redis_store, Redis.cache_configuration
+  end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
