4e300bbc90e0cc3c2832e663a6465cb8b5019100
Robin Dunlop <robin@dunlopweb.com>
2014-11-21 23:16:50 -0600

    redis config initializer call

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require File.expand_path('../../config/initializers/redis', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
