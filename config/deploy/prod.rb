70aea7c2e32b19a2a3515f091b4d6f275248afaf
Robin Dunlop <robin@dunlopweb.com>
2014-11-23 17:51:10 -0600

    update ips for new amazon instances with elastic ips

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,4 +2,4 @@ set :deploy_to, '/home/ec2-user/unicycling-registration'
 set :rails_env, 'production'
 set :branch, 'master'
 
-server '54.148.148.88', user: 'ec2-user', roles: %w(web app db)
+server '54.148.245.79', user: 'ec2-user', roles: %w(web app db)
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,4 +2,4 @@ set :deploy_to, '/home/ec2-user/unicycling-registrationtest'
 set :rails_env, 'stage'
 set :branch, 'master'
 
-server '54.148.148.88', user: 'ec2-user', roles: %w(web app db)
+server '54.68.173.23', user: 'ec2-user', roles: %w(web app db)
