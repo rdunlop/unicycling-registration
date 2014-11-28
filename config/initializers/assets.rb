153f04045681ee8db064efe2fa0a2b4dcfd8dafc
Scott Wilton <scott.o.wilton@gmail.com>
2014-11-28 16:29:57 +0100

    Styling changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,5 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
   # Additional assets to precompile
-  config.assets.precompile += %w( pdf.css dev.css naucc_2013.css naucc_2014.css unicon_17.css )
+  config.assets.precompile += %w( pdf.css dev.css naucc_2013.css naucc_2014.css naucc_2015.css unicon_17.css )
 end
