ff3a646e122d0b1b123d073d0d4dc0676ea17cc8
Robin Dunlop <robin@dunlopweb.com>
2014-12-05 19:13:04 -0600

    change the way that the menu behaves, to make it better on smaller screens. Still needs a hamburger though

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,26 +1,23 @@
 
 $(document).ready(function(){
-  $('nav > ul > li').mouseover(function() {
+  $('nav > ul > li').click(function(ev) {
+    $(".hovered").removeClass("hovered");
     $(this).addClass("hovered");
-    var parent_center = $(this).offset().left + ($(this).width() /2);
     var submenu = $(this).find("ul");
     submenu.addClass("hovered");
-    var submenu_left = parent_center - (submenu.width()/2);
-    if (submenu_left < 0) {
-      var width_reduce = ((-submenu_left) * 2);
-      submenu.css("max-width", "" + (submenu.width() - width_reduce) + "px");
-    }
-    submenu.css("left", "" + submenu_left + "px");
+
+    // remove the 'current page' if it is selected
     var cur_page = $(".current_page");
     cur_page.addClass("prev_page");
     cur_page.removeClass("current_page");
     var par_page = $(".parent_page");
     par_page.addClass("prev_parent");
     par_page.removeClass("parent_page");
+    ev.stopPropagation();
   });
-  $('nav > ul > li').mouseout(function() {
-    $(this).removeClass("hovered");
-    $(this).find("ul").removeClass("hovered");
+  // click somewhere else
+  $('body').click(function() {
+    $(".hovered").removeClass("hovered");
     var cur_page = $(".prev_page");
     cur_page.addClass("current_page");
     cur_page.removeClass("prev_page");
