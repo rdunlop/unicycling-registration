f7220da0bdd9867d9482639fb89279f84fa9cc17
Robin Dunlop <robin@dunlopweb.com>
2015-03-01 21:39:39 -0600

    attempt to fix robots.txt

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:rdunlop/unicycling-registration.git'
 set :stages, %w(prod)
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml public/robots.txt}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/sitemaps}
