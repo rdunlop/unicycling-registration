1ebe97cab29b609935be51349b2c8ca263699aac
Robin Dunlop <robin@dunlopweb.com>
2015-03-14 15:54:22 -0500

    fix event_configuration spec failure from removing currency

--- spec/views/event_configurations/payment_settings.html.erb_spec.rb
+++ spec/views/event_configurations/payment_settings.html.erb_spec.rb
@@ -11,7 +11,7 @@ describe "event_configurations/payment_settings" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => event_configuration_path, :method => "post" do
-      assert_select "input#event_configuration_currency", :name => "event_configuration[currency]"
+      assert_select "select#event_configuration_currency_code", :name => "event_configuration[currency_code]"
     end
   end
 end
