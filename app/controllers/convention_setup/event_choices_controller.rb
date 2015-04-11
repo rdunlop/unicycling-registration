7c83ad1151d1ae4ea488fa892f2a3d5a55b5862e
Robin Dunlop <robin@dunlopweb.com>
2015-04-10 22:43:30 -0500

    clean up the event edit form

--- app/controllers/convention_setup/event_choices_controller.rb
+++ app/controllers/convention_setup/event_choices_controller.rb
@@ -61,7 +61,7 @@ class ConventionSetup::EventChoicesController < ConventionSetupController
 
   def set_breadcrumbs
     add_breadcrumb "Event Categories", convention_setup_categories_path
-    add_breadcrumb "#{@event.category} Events", convention_setup_event_path(@event) if @event
+    add_breadcrumb "#{@event.category} Events", convention_setup_category_events_path(@event.category) if @event
     add_breadcrumb "Event Choices", convention_setup_event_event_choices_path(@event) if @event
   end
 
