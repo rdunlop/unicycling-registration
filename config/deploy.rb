55d76084f43adde17a38532b300403c58bcb7480
Robin Dunlop <robin@dunlopweb.com>
2015-06-14 00:33:11 -0500

    upgrade capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.3.5'
+lock '3.4.0'
 
 set :application, 'unicycling-registration'
 set :repo_url, 'git@github.com:rdunlop/unicycling-registration.git'
