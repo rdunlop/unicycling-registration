a94297cd2817b058e6a7bea4c275c8751d4aa180
Robin Dunlop <robin@dunlopweb.com>
2015-07-01 21:28:58 -0500

    fix registrant_group page

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,7 +2,7 @@
 module FormHelper
   def setup_registrant_group(registrant_group)
     registrant_group ||= RegistrantGroup.new
-    (Registrant.all - registrant_group.registrant_group_members.map {|rgm| rgm.registrant }).each do |registrant|
+    (Registrant.active.all - registrant_group.registrant_group_members.map {|rgm| rgm.registrant }).each do |registrant|
       registrant_group.registrant_group_members.build(registrant_id: registrant.id)
     end
     # registrant_group.registrant_group_members.sort_by! {|x| x.registrant.bib_number }
