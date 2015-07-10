90c185100c1f78a90edfd3d4311bf5e76c7d5b13
Robin Dunlop <robin@dunlopweb.com>
2015-07-10 17:34:09 -0500

    add ability to deploy an arbitrary branch

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,5 +1,5 @@
 set :deploy_to, '/home/ec2-user/unicycling-registrationtest'
 set :rails_env, 'stage'
-set :branch, 'master'
+set :branch, ENV['BRANCH'] || 'master'
 
 server '54.68.173.23', user: 'ec2-user', roles: %w(web app db)
