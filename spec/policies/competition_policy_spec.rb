56b7cd02f16712374e32a9224df6fa1ff578c1cd
Robin Dunlop <robin@dunlopweb.com>
2015-07-11 00:53:37 -0500

    add policies spec for competition lane_assignments

---
+++ spec/policies/competition_policy_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe CompetitionPolicy do
+  let(:competition) { FactoryGirl.create(:competition) }
+
+  subject { described_class }
+
+  permissions :manage_lane_assignments? do
+    it "denies access to normal user" do
+      expect(subject).not_to permit(FactoryGirl.create(:user), competition)
+    end
+
+    it "grants access to super_admin" do
+      expect(subject).to permit(FactoryGirl.create(:super_admin_user), competition)
+    end
+
+    it "grants access to race official for this competition" do
+      user = FactoryGirl.create(:user)
+      user.add_role :race_official, competition
+      expect(subject).to permit(user, competition)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ require 'rspec/rails'
 require 'cancan/matchers'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+require "pundit/rspec"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
