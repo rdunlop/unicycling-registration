d02173e9bf2d0fc1848c7fb77cc2f16f0665df4f
Robin Dunlop <robin@dunlopweb.com>
2015-07-12 19:49:58 -0500

    disable more standard skill tests

--- spec/controllers/standard_skill_entries_controller_spec.rb
+++ spec/controllers/standard_skill_entries_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe StandardSkillEntriesController do
+xdescribe StandardSkillEntriesController do
   before do
     @user = FactoryGirl.create(:user)
     sign_in @user
