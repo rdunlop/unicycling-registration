a5c668a5c9220d0de71312c52cdd06e69f9d151e
Robin Dunlop <robin@dunlopweb.com>
2015-07-16 23:36:33 -0500

    replace cancan exception with pundit exception in admin-upgrade

--- app/controllers/admin_upgrades_controller.rb
+++ app/controllers/admin_upgrades_controller.rb
@@ -6,7 +6,7 @@ class AdminUpgradesController < ApplicationController
   end
 
   def create
-    raise CanCan::AccessDenied.new("Incorrect Access code") unless params[:access_code] == @tenant.admin_upgrade_code
+    raise Pundit::NotAuthorizedError.new("Incorrect Access code") unless params[:access_code] == @tenant.admin_upgrade_code
 
     current_user.add_role :convention_admin
     flash[:notice] = "Successfully upgraded to convention_admin"
