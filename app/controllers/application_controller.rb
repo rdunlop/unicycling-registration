6b945f7ca27f161b2fb6ceeda6bdf49f6107fd7d
Robin Dunlop <robin@dunlopweb.com>
2015-07-19 11:56:20 -0500

    fix specs re: redirect-on-unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -101,7 +101,11 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || "/")
+    if rails_admin_controller?
+      redirect_to(request.referrer || "/")
+    else
+      redirect_to(request.referrer || root_path)
+    end
   end
 
   def locale_parameter
