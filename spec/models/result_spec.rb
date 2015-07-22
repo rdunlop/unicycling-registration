f606478e7c93b9ae94db0759210951db1c5da163
Robin Dunlop <robin@dunlopweb.com>
2015-07-21 21:11:17 -0500

    move specs from award_label into result

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -37,3 +37,85 @@ describe Result do
     expect(result2_overall).to be_valid
   end
 end
+
+describe Result do
+  before(:each) do
+    @al = FactoryGirl.build_stubbed(:result, place: 2)
+  end
+  describe "with a solo competitor" do
+    before(:each) do
+      @comp = FactoryGirl.build_stubbed(:event_competitor)
+      allow(@comp).to receive_message_chain(:members, :count).and_return(1)
+      allow(@comp).to receive_message_chain(:members, :size).and_return(1)
+      allow(@comp).to receive_message_chain(:members, :empty?).and_return(false)
+      allow(@comp).to receive(:result).and_return("Some Result")
+      @reg = FactoryGirl.build_stubbed(:competitor)
+      allow(@comp).to receive(:registrants).and_return([@reg])
+      allow(@al).to receive(:competitor).and_return(@comp)
+    end
+
+    it "Can name the registrant(s)" do
+      expect(@al.competitor_name(@reg)).to eq("#{@reg.first_name} #{@reg.last_name}")
+    end
+
+    it "displays both names if in a pair" do
+      @reg2 = FactoryGirl.build_stubbed(:competitor, first_name: "Bob", last_name: "Smith")
+      allow(@comp).to receive_message_chain(:members, :count).and_return(2)
+      allow(@comp).to receive(:registrants).and_return([@reg, @reg2])
+      expect(@al.competitor_name(@reg)).to eq("#{@reg.first_name} #{@reg.last_name} & #{@reg2.first_name} #{@reg2.last_name}")
+    end
+
+    it "displays the award_title_name as line 2 for freestyle events" do
+      competition = @comp.competition
+      competition.scoring_class = "Freestyle"
+      competition.name = "Hello"
+      competition.award_title_name = "Individual"
+      competition.save!
+
+      expect(@al.competition_name).to eq("Individual")
+    end
+
+    it "displays the award subtitle as line 4" do
+      competition = @comp.competition
+      competition.name = "Club Freestyle Int"
+      competition.award_title_name = "Club Freestyle"
+      competition.award_subtitle_name = "Intermediate Club"
+      competition.save!
+
+      expect(@al.category_name).to eq("Intermediate Club")
+    end
+
+    it "displays the team name as line 3" do
+      @comp.custom_name = "Robin Team"
+      expect(@al.team_name).to eq("Robin Team")
+    end
+
+    describe "when the competitor has expert results" do
+      before(:each) do
+        @comp.competition.scoring_class = "Shortest Time"
+        @comp.competition.age_group_type = FactoryGirl.create(:age_group_type)
+      end
+
+      it "uses the age group and the gender as line 4" do
+        allow(@comp).to receive(:age_group_entry_description).and_return("This is the age group")
+        #@al.populate_from_competitor(@comp, @reg, 3, false)
+        expect(@al.category_name).to eq("This is the age group")
+      end
+
+      it "sets the age group to Expert Male when expert" do
+        allow(@comp.competition).to receive(:has_experts?).and_return(true)
+        allow(@al).to receive(:age_group_type?).and_return(false)
+        #@al.populate_from_competitor(@comp, @reg, 3, true)
+        expect(@al.category_name).to eq("Expert Male")
+      end
+    end
+
+    it "stores the competitor.result as details, and line 5" do
+      expect(@al.details).to eq("Some Result")
+    end
+
+    it "sets the award place from the competitor" do
+      expect(@al.place).to eq(2)
+    end
+  end
+end
