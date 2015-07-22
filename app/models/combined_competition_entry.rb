81157a04ec4e11651c56dcca463f64379272d3fb
Robin Dunlop <robin@dunlopweb.com>
2015-07-21 22:53:42 -0500

    fix overall competition calculator

--- app/models/combined_competition_entry.rb
+++ app/models/combined_competition_entry.rb
@@ -68,7 +68,7 @@ class CombinedCompetitionEntry < ActiveRecord::Base
   def determine_best_time_in_thousands(gender)
     first_place_results = competition.results.includes(:competitor).overall.where(place: 1)
     gender_comp = first_place_results.map(&:competitor).find{ |comp| comp.gender == gender}
-    gender_comp.try(:competitor_comparable_result)
+    gender_comp.try(:comparable_score)
   end
 
   private
