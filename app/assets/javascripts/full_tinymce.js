25f35d4733eb81c169394650b31e0fdcf2442a87
Robin Dunlop <robin@dunlopweb.com>
2015-10-09 21:07:52 -0500

    enhanced wysiwyg for pages controller

---
+++ app/assets/javascripts/full_tinymce.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  tinymce.init({
+    selector: ".full_tinymce",
+    theme: "modern",
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true,
+    // templates: [
+    //   {title: 'Test template 1', content: 'Test 1'},
+    //   {title: 'Test template 2', content: 'Test 2'}
+    // ]
+  });
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   before_action :skip_authorization
+  layout :determine_layout_from_params
 
   # GET /pages/:slug
   def show
     @page = Page.find_by(slug: params[:slug])
+  end
+
+  private
+
+  def determine_layout_from_params
+    "minimal" if params[:framed]
   end
 end
