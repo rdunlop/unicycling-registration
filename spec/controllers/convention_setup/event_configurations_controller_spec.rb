01c0912b4e0a137679ca67a1a837f895f61c1b9b
Robin Dunlop <robin@dunlopweb.com>
2016-01-11 22:10:36 -0600

    improve documentation of more convention setup admin pages

--- spec/controllers/convention_setup/event_configurations_controller_spec.rb
+++ spec/controllers/convention_setup/event_configurations_controller_spec.rb
@@ -50,6 +50,13 @@ describe ConventionSetup::EventConfigurationsController do
     sign_in @user
   end
 
+  ConventionSetup::EventConfigurationsController::EVENT_CONFIG_PAGES.each do |page|
+    it "can render the #{page} page" do
+      get page
+      expect(response).to be_success
+    end
+  end
+
   # This should return the minimal set of attributes required to create a valid
   # EventConfiguration. As you add validations to EventConfiguration, be sure to
   # update the return value of this method accordingly.
