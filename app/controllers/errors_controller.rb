74001fdfae0731542f528ec69b5e4bdb7305af29
Robin Dunlop <robin@dunlopweb.com>
2016-02-01 22:44:53 -0600

    only allow certain request formats for the error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,7 @@
 class ErrorsController < ApplicationController
   include Gaffe::Errors
   before_action :skip_authorization
+  before_action :set_default_content_type
 
   def not_found
     if @tenant.try(:persisted?)
@@ -16,5 +17,21 @@ class ErrorsController < ApplicationController
 
   def internal_server_error
     render status: 500, layout: false
+  end
+
+  private
+
+  # Define content types that this controller responds to.
+  #
+  # The built-in responds_to was not working, so this method is used by a new
+  # before action `filter_content_types` to replace it. Override this method
+  # in controllers to return a list of any of:
+  # :html, :css, :js, :json, :pdf, :all
+  def respond_to_formats
+    [:html, :all]
+  end
+
+  def set_default_content_type
+    request.format = :html if respond_to_formats.exclude? request.format.symbol
   end
 end
