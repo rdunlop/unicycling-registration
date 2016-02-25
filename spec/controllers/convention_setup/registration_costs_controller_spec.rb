09c37d6acf0ac891617d1e14d2a3621f0aaacf6c
Robin Dunlop <robin@dunlopweb.com>
2016-02-24 23:32:01 -0600

    improve test coverage RegistrationCost

--- spec/controllers/convention_setup/registration_costs_controller_spec.rb
+++ spec/controllers/convention_setup/registration_costs_controller_spec.rb
@@ -7,6 +7,8 @@ describe ConventionSetup::RegistrationCostsController do
     @comp_exp = FactoryGirl.create(:expense_item, cost: 100)
   end
 
+  let(:registration_cost) { FactoryGirl.create(:registration_cost) }
+
   # This should return the minimal set of attributes required to create a valid
   # RegistrationCost. As you add validations to RegistrationCost, be sure to
   # update the return value of this method accordingly.
@@ -38,9 +40,23 @@ describe ConventionSetup::RegistrationCostsController do
 
   describe "GET index" do
     it "assigns all registration_costs as @registration_costs" do
-      registration_cost = FactoryGirl.create :registration_cost
+      registration_cost
       get :index, {}
       expect(assigns(:registration_costs)).to eq([registration_cost])
+    end
+
+    it "only lists both competitor and non-competitor" do
+      get :index, {}
+      expect(assigns(:registrant_types)).to match_array(["competitor", "noncompetitor"])
+    end
+
+    context "in a convention without noncompetitors" do
+      before { FactoryGirl.create(:event_configuration, noncompetitors: false) }
+
+      it "only lists competitor options" do
+        get :index, {}
+        expect(assigns(:registrant_types)).to eq(["competitor"])
+      end
     end
   end
 
@@ -53,7 +69,6 @@ describe ConventionSetup::RegistrationCostsController do
 
   describe "GET edit" do
     it "assigns the requested registration_cost as @registration_cost" do
-      registration_cost = FactoryGirl.create :registration_cost
       get :edit, id: registration_cost.to_param
       expect(assigns(:registration_cost)).to eq(registration_cost)
     end
@@ -99,7 +114,6 @@ describe ConventionSetup::RegistrationCostsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested registration_cost" do
-        registration_cost = FactoryGirl.create :registration_cost
         # Assuming there are no other registration_costs in the database, this
         # specifies that the RegistrationCost created on the previous line
         # receives the :update_attributes message with whatever params are
@@ -109,13 +123,11 @@ describe ConventionSetup::RegistrationCostsController do
       end
 
       it "assigns the requested registration_cost as @registration_cost" do
-        registration_cost = FactoryGirl.create :registration_cost
         put :update, id: registration_cost.to_param, registration_cost: valid_attributes
         expect(assigns(:registration_cost)).to eq(registration_cost)
       end
 
       it "redirects to the registration_cost" do
-        registration_cost = FactoryGirl.create :registration_cost
         params = valid_attributes.merge(expense_item_attributes: { id: registration_cost.expense_item.id })
         put :update, id: registration_cost.to_param, registration_cost: params
         expect(response).to redirect_to(registration_costs_path)
@@ -124,7 +136,6 @@ describe ConventionSetup::RegistrationCostsController do
 
     describe "with invalid params" do
       it "assigns the registration_cost as @registration_cost" do
-        registration_cost = FactoryGirl.create :registration_cost
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(RegistrationCost).to receive(:save).and_return(false)
         put :update, id: registration_cost.to_param, registration_cost: {name: 'fake'}
@@ -132,7 +143,6 @@ describe ConventionSetup::RegistrationCostsController do
       end
 
       it "re-renders the 'edit' template" do
-        registration_cost = FactoryGirl.create :registration_cost
         # Trigger the behavior that occurs when invalid params are submitted
         allow_any_instance_of(RegistrationCost).to receive(:save).and_return(false)
         put :update, id: registration_cost.to_param, registration_cost: {onsite: true}
@@ -143,16 +153,26 @@ describe ConventionSetup::RegistrationCostsController do
 
   describe "DELETE destroy" do
     it "destroys the requested registration_cost" do
-      registration_cost = FactoryGirl.create :registration_cost
+      registration_cost
       expect do
         delete :destroy, id: registration_cost.to_param
       end.to change(RegistrationCost, :count).by(-1)
     end
 
     it "redirects to the registration_costs list" do
-      registration_cost = FactoryGirl.create :registration_cost
       delete :destroy, id: registration_cost.to_param
       expect(response).to redirect_to(registration_costs_url)
+    end
+
+    context "with a non-deletable expense_item (due to payment_detail)" do
+      let(:expense_item) { FactoryGirl.create(:expense_item) }
+      let!(:payment_detail) { FactoryGirl.create(:payment_detail, expense_item: expense_item) }
+      let(:registration_cost) { FactoryGirl.create(:registration_cost, expense_item: expense_item) }
+
+      it "cannot delete the registration_cost" do
+        expect { delete :destroy, id: registration_cost.to_param }.to raise_error(ActiveRecord::DeleteRestrictionError)
+        expect(RegistrationCost.find_by(id: registration_cost.id)).not_to be_nil
+      end
     end
   end
 end
