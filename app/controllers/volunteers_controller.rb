3d1e3a81e39adad41350cf5a57dc98abc51a6eec
Robin Dunlop <robin@dunlopweb.com>
2016-03-19 16:53:55 -0500

    add specs for volunteers controller. fix issue with volunteer_type validation

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -8,7 +8,8 @@ class VolunteersController < ApplicationController
   before_action :load_competition
   before_action :authorize_competition
 
-  before_action :validate_volunteer_type, only: [:create, :destroy]
+  before_action :load_user, only: [:create, :destroy]
+  before_action :load_and_validate_volunteer_type, only: [:create, :destroy]
 
   def index
     add_to_competition_breadcrumb(@competition)
@@ -18,13 +19,6 @@ class VolunteersController < ApplicationController
   # POST /competitions/#/volunteers/:volunteer_type
   # Allows setting of roles specific to a particular competition
   def create
-    unless params[:user_id].present?
-      flash[:alert] = "Please choose a user"
-      redirect_to :back
-      return
-    end
-
-    @user = User.find(params[:user_id])
     if @user.add_role(@volunteer_type, @competition)
       flash[:notice] = "#{@competition} #{@volunteer_type} was successfully created."
       redirect_to competition_volunteers_path(@competition)
@@ -37,7 +31,6 @@ class VolunteersController < ApplicationController
 
   # DELETE
   def destroy
-    @user = User.find(params[:user_id])
     if @user.remove_role(@volunteer_type, @competition)
       flash[:notice] = "#{@competition} #{@volunteer_type} was successfully removed."
       redirect_to competition_volunteers_path(@competition)
@@ -50,10 +43,21 @@ class VolunteersController < ApplicationController
 
   private
 
+  def load_user
+    @user = User.find_by(id: params[:user_id])
+    if @user.nil?
+      flash[:alert] = "Please choose a user"
+      redirect_to :back
+    end
+  end
+
   # ensure that the given volunteer type is valid/allowed
-  def validate_volunteer_type
+  def load_and_validate_volunteer_type
     @volunteer_type = params[:volunteer_type]
-    @volunteer_type == "race_official"
+    unless @volunteer_type == "race_official"
+      flash[:alert] = "Unsupported volunteer type"
+      redirect_to :back
+    end
   end
 
   def load_competition
---
+++ spec/controllers/volunteers_controller_spec.rb
@@ -0,0 +1,91 @@
+# == Schema Information
+#
+# Table name: competitors
+#
+#  id                       :integer          not null, primary key
+#  competition_id           :integer
+#  position                 :integer
+#  custom_name              :string(255)
+#  created_at               :datetime
+#  updated_at               :datetime
+#  status                   :integer          default(0)
+#  lowest_member_bib_number :integer
+#  geared                   :boolean          default(FALSE), not null
+#  riding_wheel_size        :integer
+#  notes                    :string(255)
+#  wave                     :integer
+#  riding_crank_size        :integer
+#
+# Indexes
+#
+#  index_competitors_event_category_id  (competition_id)
+#
+
+require 'spec_helper'
+
+describe VolunteersController do
+  let(:user) { FactoryGirl.create(:user) }
+  before(:each) do
+    @ev = FactoryGirl.create(:event)
+    @ec = FactoryGirl.create(:timed_competition, event: @ev)
+
+    @ec.save!
+    user.add_role :director, @ev
+    sign_in user
+  end
+
+  # This should return the minimal set of attributes required to create a valid
+  # Competitor. As you add validations to Competitor, be sure to
+  # update the return value of this method accordingly.
+  def valid_attributes
+    { notes: "hello" }
+  end
+
+  describe "GET index" do
+    it "renders the page" do
+      get :index, competition_id: @ec.id
+      expect(response).to be_success
+    end
+  end
+
+  describe "POST create" do
+    let!(:other_user) { FactoryGirl.create(:user) }
+    before { request.env["HTTP_REFERER"] = competition_path(@ec) }
+    describe "with valid params" do
+      it "creates a new Volunteer" do
+        expect do
+          post :create, competition_id: @ec.id, user_id: other_user.id, volunteer_type: "race_official"
+        end.to change(other_user.roles, :count).by(1)
+      end
+    end
+
+    describe "with missing user_id" do
+      it "returns the user to the competition page" do
+        post :create, competition_id: @ec.id, volunteer_type: "race_official"
+        expect(response).to redirect_to(competition_path(@ec))
+      end
+    end
+
+    describe "with invalid volunteer_type" do
+      it "does something" do
+        expect do
+          post :create, competition_id: @ec.id, user_id: other_user.id, volunteer_type: "big_boss"
+        end.not_to change(other_user.roles, :count)
+        expect(response).to redirect_to(competition_path(@ec))
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      other_user.add_role(:race_official, @ec)
+    end
+
+    it "removes the role" do
+      expect do
+        delete :destroy, competition_id: @ec.id, user_id: other_user.id, volunteer_type: "race_official"
+      end.to change(other_user.roles, :count).by(-1)
+    end
+  end
+end
