7c249c959bf73cc680d200f93928b0e1793c3063
Robin Dunlop <robin@dunlopweb.com>
2016-04-23 15:47:50 -0500

    prevent sending 'non-matching-reg-item'  e-mails when marking a pending payment as complete

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -45,28 +45,8 @@ class Payment < ActiveRecord::Base
   after_save :touch_payment_details
   after_save :inform_of_coupons
 
-  def set_invoice_id
-    self.invoice_id ||= SecureRandom.hex(10)
-  end
-
   def completed_or_offline?
     completed? || offline_pending?
-  end
-
-  def touch_payment_details
-    payment_details.each do |pd|
-      pd.touch
-    end
-  end
-
-  def just_completed_or_offline_payment?
-    (completed? && completed_changed?) || (offline_pending? && offline_pending_changed?)
-  end
-
-  def inform_of_coupons
-    return true unless just_completed_or_offline_payment?
-
-    payment_details.map(&:inform_of_coupon)
   end
 
   def details
@@ -102,35 +82,6 @@ class Payment < ActiveRecord::Base
     if completed?
       if details.nil?
         errors[:base] << "Transaction ID or Note must be filled in"
-      end
-    end
-  end
-
-  def update_registrant_items
-    return true unless just_completed_or_offline_payment?
-
-    payment_details.each do |pd|
-      rei = RegistrantExpenseItem.find_by(registrant_id: pd.registrant.id, expense_item_id: pd.expense_item.id, free: pd.free, details: pd.details)
-      unless pd.details.nil?
-        if rei.nil? && pd.details.empty?
-          rei = RegistrantExpenseItem.find_by(registrant_id: pd.registrant.id, expense_item_id: pd.expense_item.id, free: pd.free, details: nil)
-        end
-      end
-
-      if rei.nil?
-        # this is used when PayPal eventually approves a payment, but the registration
-        # period has moved forward, and we have changed the associated registration_item?
-        all_reg_items = RegistrationCost.all_registration_expense_items
-        if all_reg_items.include?(pd.expense_item)
-          # the pd is a reg_item, see if there is another reg_item in the registrant's list
-          rei = pd.registrant.registration_item
-        end
-      end
-
-      if rei.nil?
-        PaymentMailer.missing_matching_expense_item(id).deliver_later
-      else
-        rei.destroy
       end
     end
   end
@@ -210,5 +161,68 @@ class Payment < ActiveRecord::Base
       free: false
     )
     payment
+  end
+
+  private
+
+  def set_invoice_id
+    self.invoice_id ||= SecureRandom.hex(10)
+  end
+
+  def touch_payment_details
+    payment_details.each do |pd|
+      pd.touch
+    end
+  end
+
+  # When a payment is completed (or marked as offline_pending)
+  # we want to remove the matching RegistrantExpenseItems, which
+  # results in "Locking" the registrant to this payment/combination
+  def update_registrant_items
+    return true if offline_payment_now_completed?
+    return true unless just_completed_or_offline_payment?
+
+    payment_details.each do |pd|
+      rei = RegistrantExpenseItem.find_by(registrant_id: pd.registrant.id, expense_item_id: pd.expense_item.id, free: pd.free, details: pd.details)
+      unless pd.details.nil?
+        if rei.nil? && pd.details.empty?
+          rei = RegistrantExpenseItem.find_by(registrant_id: pd.registrant.id, expense_item_id: pd.expense_item.id, free: pd.free, details: nil)
+        end
+      end
+
+      if rei.nil?
+        # this is used when PayPal eventually approves a payment, but the registration
+        # period has moved forward, and we have changed the associated registration_item?
+        all_reg_items = RegistrationCost.all_registration_expense_items
+        if all_reg_items.include?(pd.expense_item)
+          # the pd is a reg_item, see if there is another reg_item in the registrant's list
+          rei = pd.registrant.registration_item
+        end
+      end
+
+      if rei.nil?
+        PaymentMailer.missing_matching_expense_item(id).deliver_later
+      else
+        rei.destroy
+      end
+    end
+  end
+
+  # Send an e-mail that a coupon was used, for any payment_details which used coupons
+  def inform_of_coupons
+    return true if offline_payment_now_completed?
+    return true unless just_completed_or_offline_payment?
+
+    payment_details.map(&:inform_of_coupon)
+  end
+
+  # Has this pamyent just transitioned into offline_payment or into completed?
+  def just_completed_or_offline_payment?
+    (completed? && completed_changed?) || (offline_pending? && offline_pending_changed?)
+  end
+
+  # has an offline payment now transitioned to completed?
+  def offline_payment_now_completed?
+    (completed? && completed_changed? && offline_pending_was?)
   end
 end
