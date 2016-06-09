c83e59444366cfb7567dc18c4d3c08a98f71be2b
Robin Dunlop <robin@dunlopweb.com>
2016-06-08 20:52:11 -0500

    renew certificate weekly for production, instead of monthly

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,13 +2,6 @@ every 1.day, at: '12am', roles: [:db] do
   rake "update_registration_period"
 end
 
-case @environment
-when "production"
-  every 1.month, roles: [:db] do
-    rake "renew_and_update_certificate"
-  end
-when "stage"
-  every 1.day, roles: [:db] do
-    rake "renew_and_update_certificate"
-  end
+every 1.week, roles: [:db] do
+  rake "renew_and_update_certificate"
 end
