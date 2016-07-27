86d04966db38c2886d46872f611aec1a8bad758b
Robin Dunlop <robin@dunlopweb.com>
2016-07-27 23:25:30 +0200

    add comment for Jumps Finals calculator

--- lib/distance_attempt_managers/distance_attempt_final_2015_manager.rb
+++ lib/distance_attempt_managers/distance_attempt_final_2015_manager.rb
@@ -1,3 +1,6 @@
+# Competitor must jump at the same distance as their previous fault
+# Once a Competitor faults 3 attempts at the same distance, they are done, and their previous successful distance stands.
+# After a fault, the next attempt must be at the same distance (no further), until they succeed, or are finished.
 class DistanceAttemptFinal_2015_Manager < DistanceAttemptManager
   def no_more_jumps?
     triple_fault?
