11c85d5e1476036b80a1fe19a7cce1fb6d953fda
Robin Dunlop <robin@dunlopweb.com>
2016-07-31 14:01:58 +0200

    update schema

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,30 +2,31 @@
 #
 # Table name: competitions
 #
-#  id                            :integer          not null, primary key
-#  event_id                      :integer
-#  name                          :string(255)
-#  created_at                    :datetime
-#  updated_at                    :datetime
-#  age_group_type_id             :integer
-#  has_experts                   :boolean          default(FALSE), not null
-#  scoring_class                 :string(255)
-#  start_data_type               :string(255)
-#  end_data_type                 :string(255)
-#  uses_lane_assignments         :boolean          default(FALSE), not null
-#  scheduled_completion_at       :datetime
-#  awarded                       :boolean          default(FALSE), not null
-#  award_title_name              :string(255)
-#  award_subtitle_name           :string(255)
-#  num_members_per_competitor    :string(255)
-#  automatic_competitor_creation :boolean          default(FALSE), not null
-#  combined_competition_id       :integer
-#  order_finalized               :boolean          default(FALSE), not null
-#  penalty_seconds               :integer
-#  locked_at                     :datetime
-#  published_at                  :datetime
-#  sign_in_list_enabled          :boolean          default(FALSE), not null
-#  time_entry_columns            :string           default("minutes_seconds_thousands")
+#  id                                    :integer          not null, primary key
+#  event_id                              :integer
+#  name                                  :string(255)
+#  created_at                            :datetime
+#  updated_at                            :datetime
+#  age_group_type_id                     :integer
+#  has_experts                           :boolean          default(FALSE), not null
+#  scoring_class                         :string(255)
+#  start_data_type                       :string(255)
+#  end_data_type                         :string(255)
+#  uses_lane_assignments                 :boolean          default(FALSE), not null
+#  scheduled_completion_at               :datetime
+#  awarded                               :boolean          default(FALSE), not null
+#  award_title_name                      :string(255)
+#  award_subtitle_name                   :string(255)
+#  num_members_per_competitor            :string(255)
+#  automatic_competitor_creation         :boolean          default(FALSE), not null
+#  combined_competition_id               :integer
+#  order_finalized                       :boolean          default(FALSE), not null
+#  penalty_seconds                       :integer
+#  locked_at                             :datetime
+#  published_at                          :datetime
+#  sign_in_list_enabled                  :boolean          default(FALSE), not null
+#  time_entry_columns                    :string           default("minutes_seconds_thousands")
+#  import_results_into_other_competition :boolean          default(FALSE), not null
 #
 # Indexes
 #
