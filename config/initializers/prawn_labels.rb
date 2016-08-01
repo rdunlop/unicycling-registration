0e96cfaa03aad272c456d9e2a774b16d4a698e0d
Robin Dunlop <robin@dunlopweb.com>
2016-08-01 11:10:57 +0200

    create Prawn Labels initializer

---
+++ config/initializers/prawn_labels.rb
@@ -0,0 +1,65 @@
+Prawn::Labels.types = {
+  "Spanish4716" => {
+    "paper_size" => "A4",
+    "top_margin" => 28.09,
+    "bottom_margin" => 28.09,
+    "left_margin" => 28.09,
+    "right_margin" => 28.09,
+    "columns" => 5,
+    "rows" => 13,
+    "column_gutter" => 20,
+    "row_gutter" => 0
+  },
+  "Avery5293" => {
+    "paper_size" => "LETTER",
+    "top_margin" => 38.23, # 0.531"
+    "bottom_margin" => 38.23, # 0.531"
+    "left_margin" => 38.23, # 0.406 "
+    "right_margin" => 29.23, # 0.406"
+    "columns" => 4,
+    "rows" => 6,
+    "column_gutter" => 20.152, # 0.391 "
+    "row_gutter" => 6.152 # 0.391"
+
+    # "top_margin" => 13.49, # 0.531"
+    # "bottom_margin" => 13.49, # 0.531"
+    # "left_margin" =>  10.31, # 0.406 "
+    # "right_margin" =>  10.31, # 0.406"
+    # "columns" =>  4,
+    # "rows" =>  6,
+    # "column_gutter" =>  9.93, # 0.391 "
+    # "row_gutter" =>  9.93 # 0.391"
+  },
+  "Avery8293" => {
+    "paper_size" => "LETTER",
+    "top_margin" => 65.23, # 0.531"
+    "bottom_margin" => 54.23, # 0.531"
+    "left_margin" => 52.23, # 0.406 "
+    "right_margin" => 40.23, # 0.406"
+    "columns" => 4,
+    "rows" => 5,
+    "column_gutter" => 55, # 0.391 "
+    "row_gutter" => 50 # 0.391"
+  },
+  "Avery5160padded" => {
+    "paper_size" => "LETTER",
+    "top_margin" => 36,
+    "bottom_margin" => 36,
+    "left_margin" => 15.822,
+    "right_margin" => 15.822,
+    "columns" => 3,
+    "rows" => 10,
+    "column_gutter" => 15,
+    "row_gutter" => 2.5 # added padding
+  },
+  "Avery5434" => {
+    "paper_size" => [288, 432], # 4x6 inch
+    "columns" => 2,
+    "rows" => 5,
+    "top_margin" => 36,      # 0.5 inch
+    "bottom_margin" => 36,   # 0.5 inch
+    "column_gutter" => 18, # 0.05 inch + 0.2 inch padding
+    "left_margin" => 41.4, # 0.475 inch + 0.1 inch padding
+    "right_margin" => 41.4, # 0.475 inch + 0.1 inch padding
+  }
+}
