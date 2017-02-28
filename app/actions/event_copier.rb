3f61c6e31b84c1708f74b05c3229b975c6a1e8c0
Robin Dunlop <robin@dunlopweb.com>
2017-02-27 20:49:32 -0600

    improve the EventCopier to handle translations, and eventCategories better

--- app/actions/event_copier.rb
+++ app/actions/event_copier.rb
@@ -8,56 +8,97 @@ class EventCopier
     @error_events = []
   end
 
-  def copy_events
-    events = nil
-    categories = nil
-    Apartment::Tenant.switch subdomain do
-      events = Event.all.includes(:event_categories, :translations, category: :translations, event_choices: :translations).load
-      categories = Category.all.includes(:translations).load
-    end
-
-    categories.each do |category|
+  def create_categories
+    source_categories.each do |category|
       new_category = category.dup
       new_category.name = category.name
+      category.translations.each do |translation|
+        new_category.translations << translation.dup
+      end
       new_category.save
     end
+  end
 
-    events.each do |event|
-      new_event = event.dup
-      new_event.name = event.name
-      new_event.category = Category.find_by(name: event.category.name)
-      if new_event.save
-        event.event_choices.each do |ec|
-          new_ec = ec.dup
-          new_ec.label = ec.label
-          new_ec.tooltip = ec.tooltip
-          new_event.event_choices << new_ec # this causes the save
-        end
+  def copy_events
+    create_categories
 
-        event.event_choices.each do |ec|
-          new_ec = new_event.event_choices.find { |search_ec| search_ec.label == ec.label }
-          if ec.optional_if_event_choice_id.present?
-            optional_ec = event.event_choices.find { |search_ec| search_ec.id == ec.optional_if_event_choice_id }
-            new_ec.optional_if_event_choice = new_event.event_choices.find_by(label: optional_ec.label)
-            new_ec.save
-          end
-          if ec.required_if_event_choice_id.present?
-            required_ec = event.event_choices.find { |search_ec| search_ec.id == ec.required_if_event_choice_id }
-            new_ec.required_if_event_choice = new_event.event_choices.find_by(label: required_ec.label)
-            new_ec.save
-          end
-        end
-
-        event.event_categories.each do |ec|
-          new_event_category = ec.dup
-          new_event_category.name = ec.name
-          new_event.event_categories << new_event_category # this causes the save
-        end
+    source_events.each do |event|
+      if create_event(event)
         @successes += 1
       else
         @errors += 1
         @error_events << event.name
       end
     end
+  end
+
+  private
+
+  def create_event(source_event)
+    new_event = source_event.dup
+    new_event.name = source_event.name
+    new_event.category = Category.find_by(name: source_event.category.name)
+
+    # Set the event_categories before saving the event, to prevent creating the default 'All' EventCategory
+    source_event.event_categories.each do |ec|
+      new_event_category = ec.dup
+      new_event_category.name = ec.name
+      ec.translations.each do |translation|
+        new_event_category.translations << translation.dup
+      end
+      new_event.event_categories << new_event_category
+    end
+
+    source_event.translations.each do |translation|
+      new_event.translations << translation.dup
+    end
+
+    if new_event.save # save the Event and EventCategories
+      source_event.event_choices.each do |ec|
+        new_ec = ec.dup
+        new_ec.label = ec.label
+        new_ec.tooltip = ec.tooltip
+        ec.translations.each do |translation|
+          new_ec.translations << translation.dup
+        end
+        new_event.event_choices << new_ec # this causes the save
+      end
+
+      source_event.event_choices.each do |ec|
+        new_ec = new_event.event_choices.find { |search_ec| search_ec.label == ec.label }
+        if ec.optional_if_event_choice_id.present?
+          optional_ec = source_event.event_choices.find { |search_ec| search_ec.id == ec.optional_if_event_choice_id }
+          new_ec.optional_if_event_choice = new_event.event_choices.find_by(label: optional_ec.label)
+          new_ec.save
+        end
+        next unless ec.required_if_event_choice_id.present?
+
+        required_ec = source_event.event_choices.find { |search_ec| search_ec.id == ec.required_if_event_choice_id }
+        new_ec.required_if_event_choice = new_event.event_choices.find_by(label: required_ec.label)
+        new_ec.save
+      end
+
+      true
+    else
+      false
+    end
+  end
+
+  def source_events
+    return @events if @events
+    Apartment::Tenant.switch subdomain do
+      @events = Event.all.includes(:translations, event_categories: :translations, category: :translations, event_choices: :translations).load
+    end
+
+    @events
+  end
+
+  def source_categories
+    @categories = nil
+    Apartment::Tenant.switch subdomain do
+      @categories = Category.all.includes(:translations).load
+    end
+
+    @categories
   end
 end
--- spec/actions/event_copier_spec.rb
+++ spec/actions/event_copier_spec.rb
@@ -11,18 +11,56 @@ describe EventCopier do
     before do
       Apartment::Tenant.switch "other" do
         event = FactoryGirl.create(:event)
-        FactoryGirl.create(:event_category, event: event)
         FactoryGirl.create(:event_choice, event: event)
       end
     end
 
-    it "initially has no events" do
-      expect(Event.count).to eq(0)
+    before { copier.copy_events }
+
+    it "copies the category" do
+      expect(Category.count).to eq(1)
+      expect(Category.first.translations.count).to eq(1)
     end
 
-    it "can copy the events" do
-      copier.copy_events
+    it "copies the events" do
       expect(Event.count).to eq(1)
+    end
+
+    it "copies the event_category" do
+      expect(EventCategory.count).to eq(1)
+    end
+
+    it "copies the event_choice" do
+      expect(EventChoice.count).to eq(1)
+    end
+  end
+
+  context "with an event with a customized set of event_categories" do
+    before do
+      Apartment::Tenant.switch "other" do
+        event = FactoryGirl.create(:event)
+        event.event_categories.first.update(name: "First Category")
+        FactoryGirl.create(:event_category, event: event, name: "Second Category")
+      end
+    end
+
+    it "can copy the event categories" do
+      copier.copy_events
+      expect(Event.first.event_categories.count).to eq(2)
+    end
+  end
+
+  context "with an event with a translated event name" do
+    before do
+      Apartment::Tenant.switch "other" do
+        event = FactoryGirl.create(:event)
+        event.translations.create(name: "Francais", locale: "fr")
+      end
+    end
+
+    it "can copy the event name translations" do
+      copier.copy_events
+      expect(Event.first.translations.count).to eq(2)
     end
   end
 end
