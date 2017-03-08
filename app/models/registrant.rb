efd6e5df5d9537e0975f3c047f78f6612dd6d551
Robin Dunlop <robin@dunlopweb.com>
2017-03-07 20:12:34 -0600

    extract a simple RegistrantPresenter

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -440,48 +440,10 @@ class Registrant < ApplicationRecord # rubocop:disable Metrics/ClassLength
     active_competitors(event).first
   end
 
-  def describe_event(event)
-    details = describe_event_hash(event)
-    description = details[:description]
+  delegate :describe_event, :describe_event_hash, :describe_additional_selection, to: :presenter
 
-    unless details[:category].nil?
-      description += " - Category: " + details[:category]
-    end
-
-    unless details[:additional].nil?
-      description += " - " + details[:additional]
-    end
-    description
-  end
-
-  def describe_event_hash(event)
-    results = {}
-    results[:description] = event.name
-
-    resu = signed_up_events.detect { |sue| sue.event_id == event.id }
-    # only add the Category if there are more than 1
-    results[:category] = (resu.event_category_name unless resu.nil?)
-
-    results[:additional] = describe_additional_selection(event)
-
-    results
-  end
-
-  def describe_additional_selection(event)
-    results = []
-
-    event.event_choices.each do |ec|
-      my_val = registrant_choices.find_by(event_choice_id: ec.id)
-      if my_val.present? && my_val.has_value?
-        results << ec.label + ": " + my_val.describe_value
-      end
-    end
-
-    registrant_best_times.where(event: event).find_each do |rbt|
-      results << rbt.to_s
-    end
-
-    results.join(" - ") if results.any?
+  def presenter
+    RegistrantPresenter.new(self)
   end
 
   # return true/false to show whether an expense_group has been chosen by this registrant
