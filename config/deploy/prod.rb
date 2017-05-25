edb88ccd0a256b17897566fab9f0812c3f3cc2fc
Robin Dunlop <robin@dunlopweb.com>
2017-05-24 21:39:12 -0600

    change prod deploy target

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 set :deploy_to, '/home/ec2-user/unicycling-registration'
 set :rails_env, 'production'
-set :branch, 'master'
+set :branch, 'fix_cap_deploytags'
 
 server '54.148.245.79', user: 'ec2-user', roles: %w(web app db)
