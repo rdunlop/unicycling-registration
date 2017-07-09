0d6dd2b9056ed199c1c1a0bafe97b13fc3a655e2
Robin Dunlop <robin@dunlopweb.com>
2017-07-09 09:35:30 -0500

    add spec

---
+++ spec/actions/registrant_group_manager_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe RegistrantGroupManager do
+  describe "#add_member" do
+    it "cannot be a member of another group" do
+    end
+  end
+
+  describe "#remove_member" do
+    it "removes the member" do
+    end
+  end
+
+  describe "#promote" do
+    it "does not promote non-members" do
+    end
+
+    it "does promote member" do
+    end
+  end
+
+  describe "leave_leader" do
+    it "cannot remove the last leader" do
+    end
+
+    it "can remove 2nd last leader" do
+    end
+  end
+
+  describe "#request_leader" do
+    it "cannot request leader"
+  end
+end
