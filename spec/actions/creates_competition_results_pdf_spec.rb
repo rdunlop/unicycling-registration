154797146dd99c8ea97cb811251a24338dc42bef
Robin Dunlop <robin@dunlopweb.com>
2017-07-25 08:46:31 -0500

    publish the Freestyle Summary when the competition is published

--- spec/actions/creates_competition_results_pdf_spec.rb
+++ spec/actions/creates_competition_results_pdf_spec.rb
@@ -10,6 +10,15 @@ describe CreatesCompetitionResultsPdf do
         subject.publish!
       end.to change(CompetitionResult, :count).by(1)
     end
+
+    context "with a freestyle competition" do
+      let(:competition) { FactoryGirl.create(:competition, :freestyle_2017)}
+      it "creates 2 new results" do
+        expect do
+          subject.publish!
+        end.to change(CompetitionResult, :count).by(2)
+      end
+    end
   end
 
   describe "unpublish!" do
