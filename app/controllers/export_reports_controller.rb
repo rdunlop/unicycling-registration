c706517c92e2617ef46b871e1c3e5abb476b85c9
Robin Dunlop <robin@dunlopweb.com>
2019-04-06 08:16:16 -0500

    Fix ability to use URLs in the mailer by specifying default_locale: en.
    Extract the ExportReports into a separate controller

--- app/controllers/export_reports_controller.rb
+++ app/controllers/export_reports_controller.rb
@@ -1,18 +1,22 @@
-class ExportReportController < ApplicationController
+class ExportReportsController < ApplicationController
   before_action :authenticate_user!
 
-  def send_email
+  def create
+    export_type = params[:export_type]
+
+    redirect_back(fallback_location: export_index_path, alert: "Invalid export_type") unless export_type == "results"
+
     export = Export.create!(
       exported_by: current_user,
-      export_type: "results"
+      export_type: export_type
     )
     authorize export
     ExportJob.perform_later(export.id)
     redirect_back(fallback_location: export_index_path, notice: "Email processing....")
   end
 
-  def download_file
-    export = Export.find(params[:export_report_id])
+  def show
+    export = Export.find(params[:id])
     authorize export
     send_data export.file, filename: "#{export.type}.xls"
   end
