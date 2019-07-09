60b0649da774a85a1742906975625ae173c6277f
Robin Dunlop <robin@dunlopweb.com>
2019-07-09 17:36:52 -0600

    Prevent relative links

--- app/assets/javascripts/wysiwyg.js
+++ app/assets/javascripts/wysiwyg.js
@@ -3,13 +3,14 @@
 /* Don't have access to jquery */
 tinymce.init({
   selector: ".full_tinymce",
-  plugins: "advlist autolink lists link image charmap print preview hr anchor pagebreak" +
-    "searchreplace wordcount visualblocks visualchars code fullscreen" +
-    "insertdatetime media nonbreaking save table contextmenu directionality" +
+  plugins: "advlist autolink lists link image charmap print preview hr anchor pagebreak " +
+    "searchreplace wordcount visualblocks visualchars code fullscreen " +
+    "insertdatetime media nonbreaking save table contextmenu directionality " +
     "emoticons template paste textcolor colorpicker textpattern"
   ,
   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons",
   image_advtab: true,
+  relative_urls : false,
   // templates: [
   //   {title: 'Test template 1', content: 'Test 1'},
   //   {title: 'Test template 2', content: 'Test 2'}
