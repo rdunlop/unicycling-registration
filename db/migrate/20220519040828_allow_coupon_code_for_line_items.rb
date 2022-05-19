55df938e5ed99f7bbbe3343a1a63bfd8054c0ffe
Robin Dunlop <robin@dunlopweb.com>
2022-05-19 00:03:28 -0500

    rubocop

--- db/migrate/20220519040828_allow_coupon_code_for_line_items.rb
+++ db/migrate/20220519040828_allow_coupon_code_for_line_items.rb
@@ -4,7 +4,7 @@ class AllowCouponCodeForLineItems < ActiveRecord::Migration[5.2]
     remove_index :coupon_code_expense_items, :expense_item_id
     rename_column :coupon_code_expense_items, :expense_item_id, :line_item_id
     execute "UPDATE coupon_code_expense_items SET line_item_type = 'ExpenseItem'"
-    add_index :coupon_code_expense_items, [:line_item_id, :line_item_type], name: "coupon_code_line_item"
+    add_index :coupon_code_expense_items, %i[line_item_id line_item_type], name: "coupon_code_line_item"
   end
 
   def down
