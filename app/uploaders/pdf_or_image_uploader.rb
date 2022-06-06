b8010e0eee0b7befa37a373a031ea65b93ac19b5
Robin Dunlop <robin@dunlopweb.com>
2022-06-06 08:35:49 -0500

    allow medical certs to be images or pdfs

---
+++ app/uploaders/pdf_or_image_uploader.rb
@@ -0,0 +1,23 @@
+class PdfOrImageUploader < TenantUploader
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "#{subdomain}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w[pdf jpg jpeg gif png]
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+end
