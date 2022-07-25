348d86cf20c1124855b31d07c79f38415ad6297f
Robin Dunlop <robin@dunlopweb.com>
2022-07-25 14:33:29 -0500

    Add new menu item to allow sending email to all users, all regs

---
+++ app/models/email_filters/all_user_all_reg.rb
@@ -0,0 +1,37 @@
+class EmailFilters::AllUserAllReg
+  attr_reader :arguments
+
+  def initialize(arguments = nil)
+    @arguments = arguments
+  end
+
+  def self.config
+    EmailFilters::BooleanType.new(
+      filter: "all_user_all_reg",
+      description: "All Confirmed User Accounts, and All Registrants"
+    )
+  end
+
+  def detailed_description
+    self.class.config.description
+  end
+
+  def filtered_user_emails
+    users = User.this_tenant.confirmed
+    users.map(&:email).compact.uniq
+  end
+
+  def filtered_registrant_emails
+    Registrant.all.map(&:email).compact.uniq
+  end
+
+  # object whose policy must respond to `:contact_registrants?`
+  # or nil (which uses current_user)
+  def authorization_object
+    nil
+  end
+
+  def valid?
+    true
+  end
+end
---
+++ spec/models/email_filters/all_user_all_reg_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe EmailFilters::AllUserAllReg do
+  describe "with one confirmed account, and one with unpaid registrant" do
+    before do
+      @reg_period = FactoryBot.create(:registration_cost)
+      @reg = FactoryBot.create(:competitor)
+      @user = FactoryBot.create(:user)
+    end
+
+    it "lists email addresses of all users when confirmed_accounts selected and all registrants" do
+      @filter = described_class.new
+      expect(@filter.filtered_user_emails).to match_array([@reg.user.email, @user.email])
+      expect(@filter.filtered_registrant_emails).to match_array([@reg.email])
+    end
+  end
+end
