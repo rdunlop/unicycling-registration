8e2862c9e5205ef4ffaf85caa25e1cecf0a33548
Robin Dunlop <robin@dunlopweb.com>
2022-07-27 10:08:27 -0500

    update rails to 5.2.8.1
    Allow import of Symbols, otherwise we cannot customize the
    display order of dates for different locales

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module Workspace
     # configure an ActiveJob queue Adapter
     config.active_job.queue_adapter = :sidekiq
 
+    # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
+    config.active_record.yaml_column_permitted_classes = [Symbol]
+
     config.action_dispatch.rescue_responses['Errors::TenantNotFound'] = :not_found
 
     config.generators do |g|
