95ad1c8071587454319d50321f21f99843d3d3f5
Robin Dunlop <robin@dunlopweb.com>
2022-11-02 20:37:03 -0500

    Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
