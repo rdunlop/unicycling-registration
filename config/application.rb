982178ae872fd400067d9c39bcb4268e3601cba7
Robin Dunlop <robin@dunlopweb.com>
2022-11-02 21:04:44 -0500

    replace rails/all with separate entries to avoid including active_storage railtie

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 require File.expand_path('../config/initializers/redis', __dir__)
 require "apartment/custom_console"
 
