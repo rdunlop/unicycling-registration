296d4c9803c1c0c12057f215d24ebe59671a4e49
Robin Dunlop <robin@dunlopweb.com>
2022-11-03 22:25:44 -0500

    update to Rails 7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.server_timing = true
+
   if Rails.configuration.ssl_enabled
     config.force_ssl = true
     config.to_prepare { Devise::SessionsController.force_ssl }
