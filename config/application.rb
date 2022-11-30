b3155350ecf9611770c0fee2f374679f8eccd6d9
Robin Dunlop <robin@dunlopweb.com>
2022-11-29 21:07:46 -0600

    update defaults to included version

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
-    config.load_defaults 5.0
+    config.load_defaults 6.1
     config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
