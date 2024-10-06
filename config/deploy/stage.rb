6e08a10cbd978755d0ced8ae9015054acc47d677
Robin Dunlop <robin@dunlopweb.com>
2024-10-06 10:30:11 -0500

    try to deploy to staging via circleCI

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,4 +2,5 @@ set :deploy_to, '/home/ec2-user/unicycling-registration'
 set :rails_env, 'stage'
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "main"
 
-server 'regtest.unicycling-software.com', user: 'ec2-user', roles: %w[web app db]
+# server 'regtest.unicycling-software.com', user: 'ec2-user', roles: %w[web app db] # Old Staging Server
+server '34.216.234.215', user: 'ec2-user', roles: %w[web app db] # New Staging Server
