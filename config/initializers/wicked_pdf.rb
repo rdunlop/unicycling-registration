fc04cc68051dc79991d3ffd41a9b7f213632dd19
Robin Dunlop <robin@dunlopweb.com>
2024-11-04 00:28:04 -0600

    attempt to fix hanging specs by closing pipe

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -4,3 +4,17 @@ if Rails.env.production? || Rails.env.stage?
     enable_local_file_access: true
   }
 end
+
+class WickedPdf
+  class Binary
+    def retrieve_binary_version
+      stdin, stdout, stderr = Open3.popen3(@path + ' -V') # rubocop:disable Style/StringConcatenation
+      # must close these, or else this command hangs
+      stdin.close
+      stderr.close
+      parse_version_string(stdout.gets(nil))
+    rescue StandardError
+      default_version
+    end
+  end
+end
