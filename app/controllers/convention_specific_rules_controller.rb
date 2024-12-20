b57f118615d374d9c1311f198da4576206ac0be4
Robin Dunlop <robin@dunlopweb.com>
2024-12-20 09:44:58 -0600

    Fix error when viewing a certain page without a custom rulebook specified.

--- app/controllers/convention_specific_rules_controller.rb
+++ app/controllers/convention_specific_rules_controller.rb
@@ -3,7 +3,7 @@ class ConventionSpecificRulesController < ApplicationController
 
   # redirect to s3 file, so that the link doesn't expire
   def show
-    raise ActiveRecord::NotFound unless @config.rules_file_name?
+    raise ActiveRecord::RecordNotFound unless @config.rules_file_name?
 
     redirect_to @config.rules_file_name_url
   end
