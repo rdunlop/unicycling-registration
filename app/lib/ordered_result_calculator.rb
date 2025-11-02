ba2436ccfb4c248691bc6d4049b067b0ab3079f8
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-11-02 18:26:42 +0100

    Factorize code

--- app/lib/ordered_result_calculator.rb
+++ app/lib/ordered_result_calculator.rb
@@ -33,78 +33,16 @@ class OrderedResultCalculator
   end
 
   def update_overall_results
-    competitors_in_sorted_order_with_ineligible = CompetitorOrderer.new(competitors, lower_is_better, use_with_ineligible_score: true).sort
-
-    if competition.score_ineligible_competitors?
-      placed_competitors = competitors_in_sorted_order_with_ineligible.map do |competitor|
-        gender_place_calc = get_place_calculator("Overall: #{competitor.gender}") # differentiate between Overall and an age group named "Male"
-        new_place = gender_place_calc.place_next(competitor.comparable_score_with_ineligible, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?)
-        { competitor: competitor, new_place: new_place }
-      end
-      Result.create_new_results!(placed_competitors, "Overall")
-    else
-      # score the ineligibles..for fake-y
-      placed_competitors = competitors_in_sorted_order_with_ineligible.map do |competitor|
-        gender_place_calc = get_place_calculator("Overall Ineligible: #{competitor.gender}") # differentiate between Overall and an age group named "Male"
-        new_place = gender_place_calc.place_next(competitor.comparable_score_with_ineligible, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?, ineligible: competitor.ineligible?)
-        if competitor.ineligible?
-          { competitor: competitor, new_place: new_place }
-        end
-      end
-      Result.create_new_results!(placed_competitors, "Overall")
-
-      # score the eligibles
-      competitors_in_sorted_order = CompetitorOrderer.new(competitors, lower_is_better, use_with_ineligible_score: false).sort
-      competitors_in_sorted_order = competitors_in_sorted_order.reject(&:ineligible?)
-      placed_competitors = competitors_in_sorted_order.map do |competitor|
-        gender_place_calc = get_place_calculator("Overall Eligible: #{competitor.gender}") # differentiate between Overall and an age group named "Male"
-        new_place = gender_place_calc.place_next(competitor.comparable_score, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?)
-        { competitor: competitor, new_place: new_place }
-      end
-      Result.create_new_results!(placed_competitors, "Overall")
-    end
+    Result.create_new_results!(compute_overall_results, "Overall")
   end
 
   private
 
   def update_age_group_entry_results_with(selected_competitors)
-    competitors_in_sorted_order_with_ineligible = CompetitorOrderer.new(selected_competitors, lower_is_better, use_with_ineligible_score: true).sort
-
-    if competition.score_ineligible_competitors?
-      placed_competitors_with_subtype = competitors_in_sorted_order_with_ineligible.map do |competitor|
-        gender_place_calc = get_place_calculator("AgeGroup: #{competitor.age_group_entry_description}")
-        new_place = gender_place_calc.place_next(competitor.comparable_score_with_ineligible, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?)
-        { competitor: competitor, new_place: new_place, subtype: competitor.age_group_entry_description }
-      end
-      placed_competitors_with_subtype.group_by { |placed_competitor| placed_competitor[:subtype] }.each do |subtype, placed_competitors|
-        Result.create_new_results!(placed_competitors, "AgeGroup", subtype)
-      end
-    else
-      # score the ineligibles..for fake-y
-      placed_competitors_with_subtype = competitors_in_sorted_order_with_ineligible.map do |competitor|
-        gender_place_calc = get_place_calculator("AgeGroup Ineligible: #{competitor.age_group_entry_description}")
-        new_place = gender_place_calc.place_next(competitor.comparable_score_with_ineligible, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?, ineligible: competitor.ineligible?)
-        if competitor.ineligible?
-          { competitor: competitor, new_place: new_place, subtype: competitor.age_group_entry_description }
-        end
-      end
-      placed_competitors_with_subtype.compact.group_by { |placed_competitor| placed_competitor[:subtype] }.each do |subtype, placed_competitors|
-        Result.create_new_results!(placed_competitors, "AgeGroup", subtype)
-      end
-
-      # score the eligibles
-      competitors_in_sorted_order = CompetitorOrderer.new(selected_competitors, lower_is_better, use_with_ineligible_score: false).sort
-      competitors_in_sorted_order = competitors_in_sorted_order.reject(&:ineligible?)
-      placed_competitors_with_subtype = competitors_in_sorted_order.map do |competitor|
-        gender_place_calc = get_place_calculator("AgeGroup Eligible: #{competitor.age_group_entry_description}")
-        new_place = gender_place_calc.place_next(competitor.comparable_score, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?)
-        { competitor: competitor, new_place: new_place, subtype: competitor.age_group_entry_description }
-      end
-
-      placed_competitors_with_subtype.group_by { |placed_competitor| placed_competitor[:subtype] }.each do |subtype, placed_competitors|
-        Result.create_new_results!(placed_competitors, "AgeGroup", subtype)
-      end
-    end
+    compute_age_group_results(selected_competitors)
+      .compact
+      .group_by { |placed_competitor| placed_competitor[:subtype] }
+      .each { |subtype, placed_competitors| Result.create_new_results!(placed_competitors, "AgeGroup", subtype) }
   end
 
   def all_competitors_in_sorted_order
@@ -113,5 +51,64 @@ class OrderedResultCalculator
 
   def competitors_in_sorted_order(selected_competitors)
     @competitors_in_sorted_order ||= CompetitorOrderer.new(selected_competitors, lower_is_better).sort
+  end
+
+  def compute_overall_results
+    competitors_in_sorted_order_with_ineligible = CompetitorOrderer.new(competitors, lower_is_better, use_with_ineligible_score: true).sort
+    if competition.score_ineligible_competitors?
+      compute_places_of_competitors_including_ineligible(competitors_in_sorted_order_with_ineligible, ->(competitor) { "Overall: #{competitor.gender}" })
+    else
+      placed_ineligible_competitors = compute_places_of_ineligible_competitors(competitors_in_sorted_order_with_ineligible, ->(competitor) { "Overall Ineligible: #{competitor.gender}" })
+      placed_eligible_competitors = compute_places_of_eligible_competitors(competitors, ->(competitor) { "Overall Eligible: #{competitor.gender}" })
+
+      placed_ineligible_competitors.concat(placed_eligible_competitors)
+    end
+  end
+
+  def compute_age_group_results(selected_competitors)
+    competitors_in_sorted_order_with_ineligible = CompetitorOrderer.new(selected_competitors, lower_is_better, use_with_ineligible_score: true).sort
+    if competition.score_ineligible_competitors?
+      compute_places_of_competitors_including_ineligible(competitors_in_sorted_order_with_ineligible, ->(competitor) { "AgeGroup: #{competitor.age_group_entry_description}" })
+    else
+      placed_ineligible_competitors = compute_places_of_ineligible_competitors(competitors_in_sorted_order_with_ineligible, ->(competitor) { "AgeGroup Ineligible: #{competitor.age_group_entry_description}" })
+      placed_eligible_competitors = compute_places_of_eligible_competitors(selected_competitors, ->(competitor) { "AgeGroup Eligible: #{competitor.age_group_entry_description}" })
+
+      placed_ineligible_competitors.concat(placed_eligible_competitors)
+    end
+  end
+
+  def compute_places_of_competitors_including_ineligible(competitors_in_sorted_order_with_ineligible, group_name_provider)
+    competitors_in_sorted_order_with_ineligible.map do |competitor|
+      compute_place_of_competitor(competitor, group_name_provider.call(competitor), true)
+    end
+  end
+
+  def compute_places_of_ineligible_competitors(competitors_in_sorted_order_with_ineligible, group_name_provider)
+    competitors_in_sorted_order_with_ineligible.map do |competitor|
+      compute_place_of_competitor(competitor, group_name_provider.call(competitor), true, only_ineligible: true)
+    end
+  end
+
+  def compute_places_of_eligible_competitors(competitors, group_name_provider)
+    competitors_in_sorted_order = CompetitorOrderer.new(competitors, lower_is_better, use_with_ineligible_score: false).sort
+    competitors_in_sorted_order = competitors_in_sorted_order.reject(&:ineligible?)
+    competitors_in_sorted_order.map do |competitor|
+      compute_place_of_competitor(competitor, group_name_provider.call(competitor), false)
+    end
+  end
+
+  def compute_place_of_competitor(competitor, age_group_desc, score_with_ineligible, only_ineligible: false)
+    gender_place_calc = get_place_calculator(age_group_desc) # differentiate between Overall and an age group named "Male"
+
+    comparable_score = if score_with_ineligible
+                         competitor.comparable_score_with_ineligible
+                       else
+                         competitor.comparable_score
+                       end
+
+    new_place = gender_place_calc.place_next(comparable_score, tie_break_points: competitor.comparable_tie_break_score, dq: competitor.disqualified?, ineligible: competitor.ineligible?)
+    if !only_ineligible || competitor.ineligible?
+      { competitor: competitor, new_place: new_place, subtype: competitor.age_group_entry_description }
+    end
   end
 end
