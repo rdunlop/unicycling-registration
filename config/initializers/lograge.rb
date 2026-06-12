8dc383b1fb72311dad2bc3a83089a1976a55124f
Robin Dunlop <robin@dunlopweb.com>
2026-06-11 22:09:59 -0500

    rubocop

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     {
       tenant: Apartment::Tenant.current,
-      request_id: event.payload[:headers]&.[]("X-Request-Id"),
+      request_id: event.payload[:headers]&.[]("X-Request-Id")
     }
   end
 end
