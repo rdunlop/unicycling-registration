67bc24ae6d54a22ed07767c52953ccc66b61d223
Robin Dunlop <robin@dunlopweb.com>
2026-06-18 21:57:46 -0500

    Update prod ip

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,4 +2,4 @@ set :deploy_to, '/home/ec2-user/unicycling-registration'
 set :rails_env, 'production'
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "main"
 
-server 'registration.unicycling-software.com', user: 'ec2-user', roles: %w[web app db] # CNAME to the single prod server
+server '35.90.235.116', user: 'ec2-user', roles: %w[web app db] # CNAME to the single prod server
