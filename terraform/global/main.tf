fe5d8ed6259dbaaf69c794adea049fd1f5429c25
Robin Dunlop <robin@dunlopweb.com>
2026-06-20 13:47:19 -0500

    Change ECR to mutable, allows using :latest tag

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 resource "aws_ecr_repository" "app" {
   name                 = "unicycling-registration"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
